d1c66d74cfd4dfc111a8f6b59243a7316cd9e40d
Mike Bourgeous <mike@mikebourgeous.com>
2020-09-16 15:10:41 -0600

    Support reading info from files with more than one audio stream.

--- lib/mb/sound/ffmpeg_input.rb
+++ lib/mb/sound/ffmpeg_input.rb
@@ -8,7 +8,7 @@ module MB
     # An input stream type that uses FFMPEG to parse an audio stream from most
     # file formats.
     class FFMPEGInput < IOInput
-      attr_reader :filename, :rate, :channels, :frames, :info
+      attr_reader :filename, :rate, :channels, :frames, :info, :raw_info
 
       # A list of metadata keys that should not be parsed numerically.
       EXCLUDED_CONVERSIONS = [
@@ -26,7 +26,9 @@ module MB
         raw_info = `ffprobe -loglevel 8 -print_format json -show_format -show_streams -select_streams a #{fnesc}`
         raise "ffprobe failed: #{$?}" unless $?.success?
 
-        convert_values(JSON.parse(raw_info, symbolize_names: true))
+        convert_values(JSON.parse(raw_info, symbolize_names: true)).tap { |h|
+          h[:streams]&.sort_by! { |s| s[:index] || 0 }
+        }
       end
 
       # For internal use by .parse_info.  Converts numbers represented as
@@ -75,26 +77,34 @@ module MB
       # file.
       #
       # +filename+ - The filename to read.
-      # +stream_id+ - The number of audio stream to read in multi-stream files (0
-      #               is the default).
+      # +stream_idx+ - The number of audio stream to read in multi-stream files
+      #                (0 is the default).  This is the order in which the
+      #                stream is listed in the file, not a format-specific
+      #                stream ID, so if a file has two video and two audio
+      #                tracks, the audio tracks will still be indexes 0 and 1.
       # +resample+ - If an integer, asks ffmpeg to resample to that rate.
       # +channels+ - If not nil, asks ffmpeg to convert the number of channels.
-      def initialize(filename, stream_id: 0, resample: nil, channels: nil)
-        raise "File is not readable" unless File.readable?(filename)
+      def initialize(filename, stream_idx: 0, resample: nil, channels: nil)
+        raise "File #{filename.inspect} is not readable" unless File.readable?(filename)
         @filename = filename
         @fnesc = filename.shellescape
 
-        raise "Stream ID must be an integer" unless stream_id.is_a?(Integer)
-        @stream_id = stream_id
+        # Get info for all streams from ffprobe so we know stream IDs, etc.
+        @raw_info = FFMPEGInput.parse_info(@filename)
 
-        # First get info for all streams from ffprobe
-        @info = FFMPEGInput.parse_info(@filename)
+        raise "Stream index must be an integer" unless stream_idx.is_a?(Integer)
+        unless @raw_info[:streams][stream_idx]
+          raise "Stream index is out of range of audio streams (0..#{@raw_info[:streams].size - 1})"
+        end
+        @info = @raw_info[:format].merge(@raw_info[:streams][stream_idx])
+        @stream_idx = stream_idx
+        @stream_id = @info[:index]&.to_i || 0
 
         if channels
           raise "Channel count must be an integer greater than 0" unless channels.is_a?(Integer) && channels > 0
           @channels = channels
         else
-          @channels = @info[:streams][stream_id][:channels]
+          @channels = @info[:channels]
           raise "Missing channels from stream info" unless @channels
         end
 
@@ -102,19 +112,17 @@ module MB
           raise "Sampling rate must be an integer greater than 0" unless resample.is_a?(Integer) && resample > 0
           @rate = resample
         else
-          @rate = @info[:streams][stream_id][:sample_rate]
+          @rate = @info[:sample_rate]
         end
 
-        start = @info[:streams][stream_id][:start_time]
+        start = @info[:start_time]
         start = 0 unless start.is_a?(Numeric)
-        duration = @info[:streams][stream_id][:duration]
+        duration = @info[:duration]
         @frames = (start + duration * @rate).ceil
-
-        # FIXME: stream_id is not being handled correctly
 
         resample_opt = resample ? "-ar '#{@rate}'" : ''
         channels_opt = channels ? "-ac '#{@channels}' -af 'aresample=matrix_encoding=dplii'" : ''
-        @pipe = IO.popen(["sh", "-c", "ffmpeg -nostdin -loglevel 8 -i #{@fnesc} #{resample_opt} #{channels_opt} -f f32le -"], "r")
+        @pipe = IO.popen(["sh", "-c", "ffmpeg -nostdin -loglevel 8 -i #{@fnesc} #{resample_opt} #{channels_opt} -map 0:#{@stream_id} -f f32le -"], "r")
 
         super(@pipe, @channels)
       end
--- spec/lib/mb/sound/ffmpeg_input_spec.rb
+++ spec/lib/mb/sound/ffmpeg_input_spec.rb
@@ -3,6 +3,11 @@ RSpec.describe MB::Sound::FFMPEGInput do
     let(:info) {
       MB::Sound::FFMPEGInput.parse_info('sounds/sine/sine_100_1s_mono.flac')
     }
+
+    let(:info_multi) {
+      MB::Sound::FFMPEGInput.parse_info('spec/test_data/two_audio_streams.mkv')
+    }
+
     it 'can read stream info from a .flac sound file' do
       expect(info).to be_a(Hash)
       expect(info[:streams][0][:duration_ts]).to eq(48000)
@@ -12,6 +17,11 @@ RSpec.describe MB::Sound::FFMPEGInput do
 
     it 'can read format info from a .flac sound file' do
       expect(info[:format][:tags][:title]).to eq('Sine 100Hz 1s mono')
+    end
+
+    it 'can read info about multiple audio streams' do
+      expect(info[:streams].length).to eq(1)
+      expect(info_multi[:streams].length).to eq(2)
     end
   end
 
@@ -27,11 +37,20 @@ RSpec.describe MB::Sound::FFMPEGInput do
     MB::Sound::FFMPEGInput.new('sounds/sine/sine_100_1s_mono.flac', resample: 44100)
   }
 
+  let(:input_multi_0) {
+    MB::Sound::FFMPEGInput.new('spec/test_data/two_audio_streams.mkv', stream_idx: 0)
+  }
+
+  let(:input_multi_1) {
+    MB::Sound::FFMPEGInput.new('spec/test_data/two_audio_streams.mkv', stream_idx: 1)
+  }
+
   describe '#initialize' do
     it 'can load and parse info from a .flac sound' do
       expect(input.frames).to eq(48000)
       expect(input.rate).to eq(48000)
       expect(input.channels).to eq(1)
+      expect(input.info[:tags][:title]).to eq('Sine 100Hz 1s mono')
     end
 
     it 'can change the number of channels' do
@@ -41,6 +60,13 @@ RSpec.describe MB::Sound::FFMPEGInput do
     it 'can change the sample rate' do
       expect(input_441.frames).to eq(44100)
       expect(input_441.rate).to eq(44100)
+    end
+
+    it 'can load a second audio stream' do
+      expect(input_multi_0.rate).to eq(48000)
+      expect(input_multi_0.channels).to eq(1)
+      expect(input_multi_1.rate).to eq(44100)
+      expect(input_multi_1.channels).to eq(2)
     end
   end
 
