af1ad9675320d96f748ca23e39a09746dc5f9aba
Mike Bourgeous <mike@mikebourgeous.com>
2020-09-17 13:58:46 -0600

    Added simple methods to read and write entire sound files at once.

--- lib/mb/sound.rb
+++ lib/mb/sound.rb
@@ -8,6 +8,34 @@ module MB
   #
   # Top-level namespace for the mb-sound library.
   module Sound
+    class FileExistsError < IOError; end
+
+    # Reads an entire sound file into an array of Numo::NArrays, one per
+    # channel.
+    #
+    # See MB::Sound::FFMPEGInput for more flexible sound input.
+    def self.read(filename)
+      input = MB::Sound::FFMPEGInput.new(filename)
+      input.read(input.frames)
+    ensure
+      input&.close
+    end
+
+    # Writes an Array of Numo::NArrays into the given sound file.  If the sound
+    # file already exists and +:overwrite+ is false, an error will be raised.
+    #
+    # See MB::Sound::FFMPEGOutput for more flexible sound output.
+    def self.write(filename, data, rate:, overwrite: false)
+      if !overwrite && File.exist?(filename)
+        raise FileExistsError, "#{filename.inspect} already exists"
+      end
+
+      output = MB::Sound::FFMPEGOutput.new(filename, rate: rate, channels: data.length)
+      output.write(data)
+    ensure
+      output&.close
+    end
+
     # Converts a Ruby Array of any nesting depth to a Numo::NArray with a
     # matching number of dimensions.  All nested arrays at a particular depth
     # should have the same size (that is, all positions should be filled).
--- spec/lib/mb/sound_spec.rb
+++ spec/lib/mb/sound_spec.rb
@@ -1,4 +1,51 @@
+require 'fileutils'
+
 RSpec.describe MB::Sound do
+  describe '.read' do
+    it 'can read a sound file' do
+      a = MB::Sound.read('sounds/sine/sine_100_1s_mono.flac')
+      expect(a.length).to eq(1)
+      expect(a[0].length).to eq(48000)
+      expect(a[0].max).to be_between(0.4, 1.0)
+    end
+  end
+
+  describe '.write' do
+    before(:each) do
+      FileUtils.mkdir_p('tmp')
+      File.unlink('tmp/sound_write_test.flac') rescue nil
+      File.unlink('tmp/sound_write_exists.flac') rescue nil
+    end
+
+    it 'can write a sound file' do
+      name = 'tmp/sound_write_test.flac'
+      MB::Sound.write(name, [Numo::SFloat[0,0.5,-0.5,0]], rate: 48000)
+      info = MB::Sound::FFMPEGInput.parse_info(name)
+    end
+
+    context 'when overwrite is false (by default)' do
+      it 'raises an error if the sound already exists' do
+        name = 'tmp/sound_write_exists.flac'
+        FileUtils.touch(name)
+        expect {
+          MB::Sound.write(name, [Numo::SFloat[0, 0.1, -0.2, 0.3]], rate: 48000)
+        }.to raise_error(MB::Sound::FileExistsError)
+      end
+    end
+
+    context 'when overwrite is true' do
+      it 'overwrites an existing file' do
+        name = 'tmp/sound_write_exists.flac'
+
+        FileUtils.touch(name)
+        expect(File.size(name)).to eq(0)
+
+        MB::Sound.write(name, [Numo::SFloat[0, 0.1, -0.2, 0.3]], rate: 48000, overwrite: true)
+        expect(File.size(name)).to be > 0
+      end
+    end
+  end
+
   describe '.array_to_narray' do
     it 'converts a 1D array' do
       expect(MB::Sound.array_to_narray([1,2,3])).to eq(Numo::NArray[1,2,3])
