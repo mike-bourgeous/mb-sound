8364ddb682776ec6916f480157ca4e0b16a9ab68
Mike Bourgeous <mike@mikebourgeous.com>
2020-09-17 17:59:50 -0600

    Have pry start inside of MB::Sound, add some more intro notes.

--- bin/prysound.rb
+++ bin/prysound.rb
@@ -13,20 +13,29 @@ $LOAD_PATH << File.expand_path('../lib', __dir__)
 
 require 'mb/sound'
 
-puts WordWrap.ww(<<-EOF, IO.console.winsize[1], true)
+puts
+puts `clear` if ARGV.include?('--clear')
 
+puts WordWrap.ww(<<-EOF, IO.console.winsize[1], true)
 \e[33;1mWelcome to the interactive sound environment!\e[0m
 
+If you're new to Ruby, see https://www.ruby-lang.org/en/documentation/quickstart/.
 If you're new to Pry, check out https://pry.github.io/.
 
 \e[3mSome things to try:\e[0m
 
-\e[1mls \e[32mMB::Sound\e[0m (that's \e[1mls\e[0m for "list") to get a list of the easiest to
-use sound functions.
+\e[1mls\e[0m (for "list") to get a list of the easiest to use sound functions.
+
+\e[1mDir['sounds/**/*.*']\e[0m to get a list of included sounds.
+
+\e[1mplay('sound_filename')\e[0m to play a sound file.
+
+\e[1mcd ::\e[0m for experienced Ruby/Pry users to leave the sound context.
 
 EOF
 
 Pry.pry(
+  MB::Sound,
   prompt: Pry::Prompt.new(:mb_sound, "The interactive sound environment's default prompt", [
     _pry_a = -> (obj, nest, pry) {
       "\1\e[36m\2#{File.basename($0)}\1\e[0m\2 \1\e[32m\2#{obj}\1\e[0;2m\2(#{nest}) > \1\e[0m\2"
