8a4ed0863396e293b8e31be45fcf9001bb4683c4
Mike Bourgeous <mike@mikebourgeous.com>
2020-12-31 17:41:30 -0700

    Add a very simple test of the DSL filter method.

--- spec/lib/mb/sound_spec.rb
+++ spec/lib/mb/sound_spec.rb
@@ -76,4 +76,18 @@ RSpec.describe MB::Sound do
       end
     end
   end
+
+  describe '.filter' do
+    it 'reduces the amplitude of high frequencies more than low frequencies' do
+      low = MB::Sound.filter(123.hz.at(0.1), frequency: 1000, quality: 0.5)
+      low_gain = low[0].abs.max / 0.1
+
+      high = MB::Sound.filter(15000.hz.at(0.1), frequency: 1000, quality: 0.5)
+      high_gain = high[0].abs.max / 0.1
+
+      expect(low_gain).to be > high_gain
+      expect(low_gain).to be > -1.db
+      expect(high_gain).to be < -30.db
+    end
+  end
 end
