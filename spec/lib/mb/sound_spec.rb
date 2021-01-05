aa27cc6bdf05ba51ac49d776f5637dbcc9ba0906
Mike Bourgeous <mike@mikebourgeous.com>
2021-01-04 17:52:37 -0700

    Filled in fft and ifft tests; some are still broken.
    Also Numo::Pocketfft still crashes sometimes.

--- spec/lib/mb/sound_spec.rb
+++ spec/lib/mb/sound_spec.rb
@@ -95,7 +95,7 @@ RSpec.describe MB::Sound do
         let(:sine_input_small) {
           tone = 12000.hz.at(1).generate(24)
           n.times do
-            tone = Numo::SFloat.cast([tone] * 24)
+            tone = Numo::DFloat.cast([tone] * 24)
           end
           tone
         }
@@ -103,50 +103,111 @@ RSpec.describe MB::Sound do
         let(:sine_input_large) {
           tone = 4000.hz.at(1).generate(48)
           n.times do
-            tone = Numo::SFloat.cast([tone] * 48)
+            tone = Numo::DFloat.cast([tone] * 48)
           end
           tone
         }
 
+        let(:small_idx) {
+            [0] * n + [6]
+        }
+
+        let(:large_idx) {
+            [0] * n + [4]
+        }
+
+        let(:cosine_input) {
+          tone = 12000.hz.with_phase(-90.degrees).at(1).generate(24)
+          n.times do
+            tone = Numo::DFloat.cast([tone] * 24)
+          end
+          tone
+        }
+
+        let(:ramp_input) {
+          tone = 4000.hz.ramp.at(1).generate(48)
+          n.times do
+            tone = Numo::DFloat.cast([tone] * 48)
+          end
+          tone
+        }
+
+        let(:all_inputs) {
+          {
+            dc_input_small: dc_input_small,
+            dc_input_large: dc_input_large,
+            sine_input_small: sine_input_small,
+            sine_input_large: sine_input_large,
+            cosine_input: cosine_input,
+            ramp_input: ramp_input,
+          }
+        }
+
         describe '.fft' do
-          it 'returns a DC value of 1.0 for an array filled with ones' do
-            expect(MB::Sound.fft(dc_input_small)[*([0] * ndim)].real.round(5)).to eq(1)
+          it 'returns a DC value of 2.0 for an array filled with ones' do
+            expect(MB::Sound.fft(dc_input_small)[*([0] * ndim)].real.round(5)).to eq(2)
             expect(MB::Sound.fft(dc_input_small)[*([1] * ndim)].real.round(5)).to eq(0)
             expect(MB::Sound.fft(dc_input_small)[*([0] * ndim)].imag.round(5)).to eq(0)
             expect(MB::Sound.fft(dc_input_small)[*([1] * ndim)].imag.round(5)).to eq(0)
-            expect(MB::Sound.fft(dc_input_small).sum.real.round(5)).to eq(1)
+            expect(MB::Sound.fft(dc_input_small).sum.real.round(5)).to eq(2)
             expect(MB::Sound.fft(dc_input_small).sum.imag.round(5)).to eq(0)
 
-            expect(MB::Sound.fft(dc_input_large)[*([0] * ndim)].real.round(5)).to eq(1)
+            expect(MB::Sound.fft(dc_input_large)[*([0] * ndim)].real.round(5)).to eq(2)
             expect(MB::Sound.fft(dc_input_large)[*([1] * ndim)].real.round(5)).to eq(0)
             expect(MB::Sound.fft(dc_input_large)[*([0] * ndim)].imag.round(5)).to eq(0)
             expect(MB::Sound.fft(dc_input_large)[*([1] * ndim)].imag.round(5)).to eq(0)
-            expect(MB::Sound.fft(dc_input_large).sum.real.round(5)).to eq(1)
+            expect(MB::Sound.fft(dc_input_large).sum.real.round(5)).to eq(2)
             expect(MB::Sound.fft(dc_input_large).sum.imag.round(5)).to eq(0)
           end
 
-          it 'returns + and - bin values of +/-0.5i for a bin-centered sinusoid' do
-            small_idx = [0] * n + [6]
+          it 'returns + and - bin values of +/-1i for a bin-centered sinusoid' do
             small_fft = MB::Sound.fft(sine_input_small)
-            expect(small_fft.abs.sum.round(6)).to eq(1)
-            expect(MB::Sound::M.round(small_fft[*small_idx], 6)).to eq(0-0.5i)
-            expect(MB::Sound::M.round(small_fft[*small_idx.map(&:-@)], 6)).to eq(0+0.5i)
+            expect(small_fft.abs.sum.round(6)).to eq(2)
+            expect(MB::Sound::M.round(small_fft[*small_idx], 6)).to eq(0-1i)
+            expect(MB::Sound::M.round(small_fft[*small_idx.map(&:-@)], 6)).to eq(0+1i)
 
-            large_idx = [0] * n + [4]
             large_fft = MB::Sound.fft(sine_input_large)
-            expect(large_fft.abs.sum.round(6)).to eq(1)
-            expect(MB::Sound::M.round(large_fft[*large_idx], 6)).to eq(0-0.5i)
-            expect(MB::Sound::M.round(large_fft[*large_idx.map(&:-@)], 6)).to eq(0+0.5i)
+            expect(large_fft.abs.sum.round(6)).to eq(2)
+            expect(MB::Sound::M.round(large_fft[*large_idx], 6)).to eq(0-1i)
+            expect(MB::Sound::M.round(large_fft[*large_idx.map(&:-@)], 6)).to eq(0+1i)
           end
 
-          pending 'returns 0 phase for a cosine'
+          it 'returns 0 phase for a cosine' do
+            fft = MB::Sound.fft(cosine_input)
+            expect(fft[*small_idx].abs.round(6)).to eq(1)
+            expect(fft[*small_idx].arg.round(6)).to eq(0)
+            expect(fft[*small_idx.map(&:-@)].abs.round(6)).to eq(1)
+            expect(fft[*small_idx.map(&:-@)].arg.round(6)).to eq(Math::PI.round(6))
+          end
 
-          pending 'returns PI/4 phase for a sine'
+          it 'returns PI/4 phase for a sine' do
+            fft = MB::Sound.fft(cosine_input)
+            expect(fft[*small_idx].abs.round(6)).to eq(1)
+            expect(fft[*small_idx].arg.round(6)).to eq((Math::PI / 2).round(6))
+            expect(fft[*small_idx.map(&:-@)].abs.round(6)).to eq(1)
+            expect(fft[*small_idx.map(&:-@)].arg.round(6)).to eq((-Math::PI / 2).round(6))
+          end
         end
 
         describe '.ifft' do
-          pending 'returns the original signal when passed the output of #fft'
-          pending 'returns an array of all ones for a 1.0 DC value'
+          it "returns the original signal when passed the output of #fft" do
+            all_inputs.each do |name, input|
+              fft = MB::Sound.fft(input)
+              inv_fft = MB::Sound.ifft(fft).real
+
+              # Including the name so the diff for failed comparisons will show the name
+              expect([name, MB::Sound::M.round(inv_fft, 6)]).to eq([name, MB::Sound::M.round(input, 6)])
+            end
+          end
+
+          it 'returns an array of all ones for a 2.0 DC value' do
+            dc_fft = Numo::DFloat.zeros([5] * ndim)
+            dc_fft[0] = 2
+            result = MB::Sound.ifft(dc_fft)
+            expect(result.sum.abs.round(6)).to eq(5 ** ndim)
+            expect(result.abs.min.round(6)).to eq(1)
+            expect(result.abs.max.round(6)).to eq(1)
+          end
         end
 
         describe '.real_fft' do
