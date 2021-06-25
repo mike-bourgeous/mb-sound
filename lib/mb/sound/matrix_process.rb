574bd0b81ade30dc79b303e3b38cf813f035e0bd
Mike Bourgeous <mike@mikebourgeous.com>
2021-06-25 14:09:41 -0600

    Added examples to the MatrixProcess documentation.

--- lib/mb/sound/matrix_process.rb
+++ lib/mb/sound/matrix_process.rb
@@ -15,11 +15,75 @@ module MB
     # The number of input streams must match the number of columns in the
     # Matrix, and the number of output streams will match the number of rows.
     #
-    # TODO: Show how this relates to equations for each channel
+    # This class is useful for producing output channels that are based on an
+    # equation that combines the input channels.  In general, a system of
+    # linear equations can be represented as a matrix.
     #
-    # TODO: Examples
+    # These equations:
     #
-    # TODO: Should there be an extra 1.0 column for translation?
+    #     f = A * x + B * y
+    #     g = C * x + D * y
+    #
+    # Can be represented by this matrix multiplication:
+    #
+    #     [ f ] = [ A  B ] * [ x ]
+    #     [ g ]   [ C  D ]   [ y ]
+    #
+    # In Ruby code form:
+    #
+    #     m = Matrix[
+    #       [ A, B ],
+    #       [ C, D ]
+    #     ]
+    #     input = Vector[x, y]
+    #     output = m * input
+    #     f, g = *output
+    #
+    # Examples:
+    #
+    #     # Pass-through of two channels
+    #     p = MB::Sound::MatrixProcess.new(
+    #       Matrix.identity(2) # Same thing as Matrix[[1, 0], [0, 1]]
+    #     )
+    #     p.process([Numo::SFloat[1, 2, 3], Numo::SFloat[4, 5, 6]])
+    #     # => [Numo::SFloat[1, 2, 3], Numo::SFloat[4, 5, 6]]
+    #
+    #     # Swap channels
+    #     p = MB::Sound::MatrixProcess.new(
+    #       Matrix[
+    #         [0, 1],
+    #         [1, 0]
+    #       ]
+    #     )
+    #     p.process([Numo::SFloat[1, 2, 3], Numo::SFloat[4, 5, 6]])
+    #     # => [Numo::SFloat[4, 5, 6], Numo::SFloat[1, 2, 3]]
+    #
+    #     # Hafler circuit to generate rear ambience from stereo
+    #     # Two input channels, four output channels
+    #     p = MB::Sound::MatrixProcess.new(
+    #       Matrix[
+    #         [1, 0], # Left
+    #         [0, 1], # Right
+    #         [1, -1], # Rear left
+    #         [-1, 1], # Rear right
+    #       ]
+    #     )
+    #     p.process([Numo::SFloat[1, 2, 3], Numo::SFloat[4, 5, 6]])
+    #     # => [Numo::SFloat[4, 5, 6], Numo::SFloat[1, 2, 3], Numo::SFloat[-3, -3, -3], Numo::SFloat[3, 3, 3]]
+    #
+    #     # ElectroVoice Stereo-4 (1970) encoder
+    #     # Four input channels, two output channels
+    #     # https://en.wikipedia.org/wiki/Stereo-4
+    #     p = MB::Sound::MatrixProcess.new(
+    #       Matrix[
+    #         [1.0, 0.3, 1.0, -0.5], # Left total
+    #         [0.3, 1.0, -0.5, 1.0], # Right total
+    #       ]
+    #     )
+    #     p.process([Numo::SFloat[1, 2, 3], Numo::SFloat[4, 5, 6], Numo::SFloat[-3, -4, -5], Numo::SFloat[-1, 0, 1]])
+    #     # => [Numo::SFloat[-0.3, -0.5, -0.7], Numo::SFloat[4.8, 7.6, 10.4]]
+    #
+    # TODO: Should there be an extra 1.0 column for translation / DC bias?
     # TODO: Allow changing the matrix?
     # TODO: Think of a better name?
     class MatrixProcess
