ebe95f48c0655655aea792ac92d9c0974918ed02
Mike Bourgeous <mike@mikebourgeous.com>
2021-06-30 17:29:43 -0600

    Copied phase_synth.rb from other repo.
    Example command line from one of the commit messages:
    
    PHASE_TRANSITION=4800 bin/phase_synth.rb tests/phase.flac 240 0 0.5 45 0.5 90 0.5 135 0.5 180 0.5 225 0.5 270 0.5 315 0.5 360 0.5 405 0.5 450 0.5 495 0.5 540 0.5 585 0.5 630 0.5 675 0.5 720 0.5
    
    Prior commits:
    
    $ gla bin/phase_synth.rb
    d39f6d2 Mike Bourgeous  2021-06-30 17:14:05 -0600       Print header comment with phase_synth usage info.
    038d7ea Mike Bourgeous  2021-06-30 16:27:28 -0600       Print header comment along with usage information.
    6ebae13 Mike Bourgeous  2021-06-20 00:08:11 -0600       Removed all RubyAudio I/O in favor of MB::Sound::FFMPEGInput/Output.
    d138cfd Mike Bourgeous  2021-04-14 20:36:32 -0600       Extracted smoothstep, smootherstep, and interp to mb-math project.
    fcffc05 Mike Bourgeous  2020-09-30 21:10:14 -0600       Added a test clip for visualizing relative phase.
    b046fa2 Mike Bourgeous  2020-09-16 11:34:51 -0600       Print usage with invalid options in phase_synth.
    bb1e9a5 Mike Bourgeous  2020-08-19 23:35:13 -0600       Added a tool to synthesize a sine wave with scheduled phase changes.

---
+++ bin/phase_synth.rb
@@ -0,0 +1,121 @@
+#!/usr/bin/env ruby
+# Synthesizes a stereo sine wave with periodic phase transitions between
+# in-phase, out-of-phase, or between.  The final phase will play for 1 second
+# unless a final delay is given.
+#
+# Delays between phases should be at least 100ms to account for phase
+# transition time.  Phase should range from 0 to 180.
+#
+# Phase transition duration may be changed using the PHASE_TRANSITION
+# environment variable, which should contain an integer number of samples.  The
+# default transition duration is 480 samples.
+#
+# Example:
+#     bin/phase_synth.rb /tmp/x.flac 300 0 1 45 1 90 1 135 1 180 1 135 1 90 1 45 1 0 1
+#
+# Output:
+#     Frequency: 300.00Hz
+#
+#     Index   Start  Phase
+#        0:       0   0.00
+#        1:   47520  45.00
+#        2:   95520  90.00
+#        3:  143520 135.00
+#        4:  191520 180.00
+#        5:  239520 135.00
+#        6:  287520  90.00
+#        7:  335520  45.00
+#        8:  383520   0.00
+#        9:  431520    END
+#
+#     Wrote 431520 samples to /tmp/x.flac
+
+require 'bundler/setup'
+
+require 'benchmark'
+require 'pry-byebug'
+
+$LOAD_PATH << File.expand_path('../lib', __dir__)
+
+require 'mb-sound'
+
+USAGE = "\n\n#{MB::U.read_header_comment.join}\n(usage: #{$0} out_file frequency [initial_phase_deg [delay_s phase_deg [delay_s phase_deg... [delay_end]]]]"
+RATE = 48000
+PHASE_TRANSITION = ENV['PHASE_TRANSITION']&.to_i || 480
+AMP_TRANSITION = 4800
+
+(puts USAGE;exit) if ARGV.empty? || ARGV.include?('--help')
+
+filename = ARGV[0]
+raise "No or invalid output file given #{USAGE}" unless filename && File.directory?(File.dirname(filename))
+output = MB::Sound::FFMPEGOutput.new(filename, rate: RATE, channels: 2)
+
+freq = ARGV[1]&.to_f
+raise "No or invalid frequency given #{USAGE}" unless freq && freq > 0
+
+phases = []
+phases << { start: 0, phase: (ARGV[2] || 0).to_f }
+ARGV.shift(3)
+
+puts "\n\e[36mFrequency: \e[1m#{'%.2f' % freq}Hz\e[0m"
+
+samples = 0
+while (delay, phase = ARGV.shift(2)).length >= 1
+  start = (samples - PHASE_TRANSITION + delay.to_f * RATE).round
+  start = samples if start < samples
+  samples = start + PHASE_TRANSITION
+  phases << {
+    start: start,
+    phase: phase&.to_f
+  }
+end
+
+phases << { start: samples + RATE, phase: nil } unless phases.last[:phase].nil?
+
+puts "\n\e[1;33mIndex \e[32m  Start  \e[34mPhase\e[0m"
+phases.each_with_index do |p, idx|
+  puts "\e[33m#{idx.to_s.rjust(4)}: \e[32m#{p[:start].to_s.rjust(7)} \e[34m#{(p[:phase] ? '%.2f' % p[:phase] : 'END').rjust(6)}\e[0m"
+end
+
+sample = 0
+phase_idx = 0
+prior_phase = phases[0][:phase]
+data = [Numo::SFloat.zeros(phases.last[:start]), Numo::SFloat.zeros(phases.last[:start])]
+while sample <= phases.last[:start] # XXX <= should be < but testing another condition
+  amp = 0.5
+
+  # Fade in at start
+  if sample < AMP_TRANSITION
+    amp *= MB::M.smootherstep(sample.to_f / AMP_TRANSITION)
+  end
+
+  # Fade out at end
+  if phases[phase_idx + 1][:phase].nil? && sample >= phases[phase_idx + 1][:start] - AMP_TRANSITION
+    amp *= MB::M.smootherstep((phases[phase_idx + 1][:start] - sample).to_f / AMP_TRANSITION)
+  end
+
+  # Transition between phases
+  if phase_idx > 0 && sample < phases[phase_idx][:start] + PHASE_TRANSITION
+    phase = MB::M.interp(prior_phase, phases[phase_idx][:phase], MB::M.smootherstep((sample - phases[phase_idx][:start]).to_f / PHASE_TRANSITION))
+  else
+    phase = phases[phase_idx][:phase]
+  end
+
+  # phase is divided by 360 for half phase in each channel
+  base_phase = sample * freq * 2.0 * Math::PI / RATE
+  data[0][sample] = amp * Math.sin(base_phase + phase * Math::PI / 360.0)
+  data[1][sample] = amp * Math.sin(base_phase - phase * Math::PI / 360.0)
+
+  sample += 1
+
+  if sample >= phases[phase_idx + 1][:start]
+    prior_phase = phase
+    phase_idx += 1
+    break if phases[phase_idx][:phase].nil?
+  end
+end
+
+output.write(data)
+output.close
+
+puts "\nWrote \e[1m#{sample}\e[0m samples to \e[1m#{filename}\e[0m"
