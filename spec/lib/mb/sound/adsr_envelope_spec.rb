2ae713b109e1a256fd8682dabd086aa1dcae99d1
Mike Bourgeous <mike@mikebourgeous.com>
2021-07-16 15:32:31 -0600

    Added tests for the ADSR envelope.

--- spec/lib/mb/sound/adsr_envelope_spec.rb
+++ spec/lib/mb/sound/adsr_envelope_spec.rb
@@ -1,3 +1,44 @@
 RSpec.describe(MB::Sound::ADSREnvelope) do
-  pending
+  let(:env) {
+    MB::Sound::ADSREnvelope.new(
+      attack_time: 0.1,
+      decay_time: 0.2,
+      sustain_level: 0.75,
+      release_time: 0.5,
+      rate: 48000
+    )
+  }
+
+  it 'produces the expected attack/decay curve and sustain level' do
+    env.trigger(2)
+    result = env.sample(48000)
+    expect(result[0].round(6)).to eq(0)
+    expect(result[4800].round(1)).to eq(2)
+    expect(result[9800].round(2)).to eq(1.75)
+    expect(result[14400].round(2)).to eq(1.5)
+    expect(result[-1].round(6)).to eq(1.5)
+  end
+
+  it 'produces the expected release curve' do
+    env.trigger(1)
+    env.sample(48000)
+    env.release
+    result = env.sample(48000)
+    expect(result[0].round(6)).to eq(0.75)
+    expect(result[12000..12500].mean.round(3)).to eq(0.375)
+    expect(result[24000].round(2)).to eq(0)
+    expect(result[-1].round(6)).to eq(0)
+  end
+
+  it 'can be released early' do
+    env.trigger(1)
+    env.sample(4800)
+    expect(env.sample.round(1)).to eq(1)
+    env.release
+    result = env.sample(30000)
+    expect(result[0].round(1)).to eq(1)
+    expect(result[12000..12500].mean.round(2)).to eq(0.5)
+    expect(result[24000].round(2)).to eq(0)
+    expect(result[-1].round(6)).to eq(0)
+  end
 end
