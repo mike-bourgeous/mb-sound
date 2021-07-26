cb52ec366867b1700f9625a619406b67d0b906f7
Mike Bourgeous <mike@mikebourgeous.com>
2021-07-26 15:55:35 -0600

    HaasPan: Added #reset_delay and per-channel accessors, added tests.

--- lib/mb/sound/haas_pan.rb
+++ lib/mb/sound/haas_pan.rb
@@ -14,16 +14,29 @@ module MB
       # Sample rate given to the constructor.
       attr_reader :rate
 
-      # Initializes a HaasPan with the given +:delay+ in seconds, and the given
-      # sample +:rate+.
+      # Initializes a HaasPan with the given relative +:delay+ in seconds
+      # (positive delays right, negative delays left), and the given sample
+      # +:rate+.
       def initialize(delay: 0, rate: 48000)
         @rate = rate.to_f
-        self.delay = delay
 
         # Two separate delays are needed, because of delay smoothing.  Without
         # delay smoothing only one delay object could be used.
-        @left_delay = MB::Sound::Filter::Delay.new(delay: @delay > 0 ? 0 : @delay, rate: rate, smoothing: true)
-        @right_delay = MB::Sound::Filter::Delay.new(delay: @delay > 0 ? @delay : 0, rate: rate, smoothing: true)
+        @left_delay = MB::Sound::Filter::Delay.new(delay: 0, rate: rate, smoothing: true)
+        @right_delay = MB::Sound::Filter::Delay.new(delay: 0, rate: rate, smoothing: true)
+
+        self.delay = delay
+
+        @left_delay.reset_delay
+        @right_delay.reset_delay
+      end
+
+      # Resets smoothing on the underlying MB::Sound::Filter::Delay objects, so
+      # that the left and right delays immediately jump to their target values,
+      # instead of gradually changing over time.
+      def reset_delay
+        @left_delay.reset_delay
+        @right_delay.reset_delay
       end
 
       # Sets the delay time in +samples+, rounded to the nearest Integer,
@@ -32,6 +45,14 @@ module MB
       def delay_samples=(samples)
         @delay_samples = samples.round
         @delay = @delay_samples.to_f / @rate
+
+        if @delay_samples >= 0
+          @left_delay.delay_samples = 0
+          @right_delay.delay_samples = @delay_samples
+        else
+          @left_delay.delay_samples = @delay_samples.abs
+          @right_delay.delay_samples = 0
+        end
       end
 
       # Sets the delay time in +seconds+, based on sample rate.  Delay time
@@ -41,20 +62,32 @@ module MB
         self.delay_samples = seconds * @rate
       end
 
-      # Processes one or two channels of data, returning two channels with the
-      # relative delay specified by #delay.
+      # Returns the delay applied to the left channel, in seconds.
+      def left_delay
+        @left_delay.delay
+      end
+
+      # Returns the delay applied to the left channel, in samples.
+      def left_delay_samples
+        @left_delay.delay_samples
+      end
+
+      # Returns the delay applied to the right channel, in seconds.
+      def right_delay
+        @right_delay.delay
+      end
+
+      # Returns the delay applied to the right channel, in samples.
+      def right_delay_samples
+        @right_delay.delay_samples
+      end
+
+      # Processes one or two channels of data, returning two channels [left,
+      # right] with the relative delay specified by #delay= or #delay_samples=.
       def process(data)
         data = [data, data.dup] if data.is_a?(Numo::NArray)
         data = [data[0], data[0].dup] if data.is_a?(Array) && data.length == 1
         raise 'Pass one or two channels to #process' if data.length != 2
-
-        if @delay_samples >= 0
-          @left_delay.delay_samples = 0
-          @right_delay.delay_samples = @delay_samples
-        else
-          @left_delay.delay_samples = @delay_samples.abs
-          @right_delay.delay_samples = 0
-        end
 
         l = @left_delay.process(data[0])
         r = @right_delay.process(data[1])
--- spec/lib/mb/sound/haas_pan_spec.rb
+++ spec/lib/mb/sound/haas_pan_spec.rb
@@ -1,5 +1,92 @@
 RSpec.describe(MB::Sound::HaasPan) do
-  it 'can be constructed' do
-    expect { MB::Sound::HaasPan.new }.not_to raise_error
+  let(:d_default) { MB::Sound::HaasPan.new }
+  let(:d_32k_r) { MB::Sound::HaasPan.new(delay: 0.1, rate: 32000) }
+  let(:d_44k1_l) { MB::Sound::HaasPan.new(delay: -0.2, rate: 44100) }
+
+  describe '#initialize' do
+    it 'can be constructed with no parameters' do
+      d = nil
+      expect { d = d_default }.not_to raise_error
+      expect(d.delay).to eq(0)
+      expect(d.rate).to eq(48000)
+      expect(d.left_delay).to eq(0)
+      expect(d.right_delay).to eq(0)
+      expect(d.left_delay_samples).to eq(0)
+      expect(d.right_delay_samples).to eq(0)
+    end
+
+    it 'can use a custom delay and rate' do
+      d = nil
+      expect { d = d_32k_r }.not_to raise_error
+      expect(d.delay).to eq(0.1)
+      expect(d.rate).to eq(32000)
+      expect(d.left_delay).to eq(0)
+      expect(d.right_delay).to eq(0.1)
+      expect(d.left_delay_samples).to eq(0)
+      expect(d.right_delay_samples).to eq(3200)
+    end
+
+    it 'can use a negative delay' do
+      d = nil
+      expect { d = d_44k1_l }.not_to raise_error
+      expect(d.delay).to eq(-0.2)
+      expect(d.rate).to eq(44100)
+      expect(d.left_delay).to eq(0.2)
+      expect(d.right_delay).to eq(0)
+      expect(d.left_delay_samples).to eq(8820)
+      expect(d.right_delay_samples).to eq(0)
+    end
+  end
+
+  describe '#process' do
+    let(:data) { Numo::SFloat.zeros(8).rand(-1, 1) }
+
+    it 'can process a single NArray' do
+      result = d_default.process(data)
+      expect(result).to eq([data, data])
+    end
+
+    it 'can process a one-element Array with a single NArray' do
+      result = d_default.process([data])
+      expect(result).to eq([data, data])
+    end
+
+    it 'can process a two-element Array of NArray' do
+      result = d_default.process([data, data])
+      expect(result).to eq([data, data])
+    end
+
+    it 'raises an error if given too many channels' do
+      expect { d_default.process([data, data, data]) }.to raise_error(/channels/)
+    end
+
+    it 'delays the right channel when delay is positive' do
+      expected = [
+        data,
+        MB::M.shr(data, 3)
+      ]
+
+      d_32k_r.delay_samples = 3
+      d_32k_r.reset_delay
+
+      result = d_32k_r.process(data)
+      expect(result).to eq(expected)
+    end
+
+    it 'delays the left channel when delay is negative' do
+      expected = [
+        MB::M.shr(data, 4),
+        data
+      ]
+
+      d_44k1_l.delay_samples = -4
+      d_44k1_l.reset_delay
+
+      result = d_44k1_l.process(data)
+      expect(result).to eq(expected)
+    end
+
+    # TODO: Does this need to be re-tested here when it's tested on Filter::Delay?
+    pending 'smooths delay changes'
   end
 end
