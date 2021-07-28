512d3374fff5858ee7fdb053e8934358c45ee05b
Mike Bourgeous <mike@mikebourgeous.com>
2021-07-28 17:51:41 -0600

    Test bin/matrix_process.rb with --decode flac and complex values.

--- spec/bin/matrix_process_spec.rb
+++ spec/bin/matrix_process_spec.rb
@@ -2,6 +2,8 @@ RSpec.describe('bin/matrix_process.rb') do
   before(:each) do
     FileUtils.mkdir_p('tmp')
     File.unlink('tmp/matrix_process_test.flac') rescue nil
+    File.unlink('tmp/matrix_process_test_qs.flac') rescue nil
+    File.unlink('tmp/matrix_process_test_qs_enc.flac') rescue nil
   end
 
   it 'can convert a 2ch file to a 4ch file' do
@@ -14,5 +16,28 @@ RSpec.describe('bin/matrix_process.rb') do
 
     expect(out_info[:streams][0][:channels]).to eq(4)
     expect(out_info[:streams][0][:duration_ts]).to eq(in_info[:streams][0][:duration_ts])
+  end
+
+  it 'can decode and re-encode using an included complex-valued matrix' do
+    text = `bin/matrix_process.rb --decode sounds/synth0.flac qs.yml tmp/matrix_process_qs.flac`
+    expect($?).to be_success
+    expect(text).to include('included matrix')
+    expect(text).to include('Success')
+
+    in_info = MB::Sound::FFMPEGInput.parse_info('sounds/synth0.flac')
+    out_info = MB::Sound::FFMPEGInput.parse_info('tmp/matrix_process_qs.flac')
+
+    expect(out_info[:streams][0][:channels]).to eq(4)
+    expect(out_info[:streams][0][:duration_ts]).to eq(in_info[:streams][0][:duration_ts])
+
+    text = `bin/matrix_process.rb tmp/matrix_process_qs.flac qs.yml tmp/matrix_process_qs_enc.flac`
+    expect($?).to be_success
+    expect(text).to include('included matrix')
+    expect(text).to include('Success')
+
+    enc_info = MB::Sound::FFMPEGInput.parse_info('tmp/matrix_process_qs_enc.flac')
+
+    expect(enc_info[:streams][0][:channels]).to eq(2)
+    expect(enc_info[:streams][0][:duration_ts]).to eq(in_info[:streams][0][:duration_ts])
   end
 end
