5c707335a597774493b3a53c9fa5c5969391fe62
Mike Bourgeous <mike@mikebourgeous.com>
2021-12-04 23:03:45 -0700

    Add a C function to calculate the natural logarithm of an NArray.
    This might be useful for exponential FM synthesis.

--- ext/mb/fast_sound/fast_sound.c
+++ ext/mb/fast_sound/fast_sound.c
@@ -847,6 +847,59 @@ static VALUE ruby_complex(VALUE self, VALUE z)
 	return rb_ary_new_from_args(2, rb_float_new(creal(c)), rb_float_new(cimag(c)));
 }
 
+/*
+ * Calculates the natural logarithm of every element in the NArray.  Leaves
+ * SFloat, SComplex, and DComplex as their original type, converts everything
+ * else to DFloat.
+ */
+static VALUE ruby_narray_log(VALUE self, VALUE narray)
+{
+	VALUE ntype = CLASS_OF(narray);
+	if (ntype != numo_cSFloat && ntype != numo_cDFloat && ntype != numo_cSComplex && ntype != numo_cDComplex) {
+		narray = rb_funcall(numo_cDFloat, rb_intern("cast"), 1, narray);
+		ntype = numo_cDFloat;
+	}
+
+	_Bool was_inplace = !!TEST_INPLACE(narray);
+	if (!RTEST(nary_check_contiguous(narray)) || !was_inplace) {
+		narray = nary_dup(narray);
+		SET_INPLACE(narray);
+		was_inplace = 0;
+	}
+
+	size_t length = RNARRAY_SIZE(narray);
+
+	if (ntype == numo_cSFloat) {
+		float *ptr = (float *)(nary_get_pointer_for_read_write(narray) + nary_get_offset(narray));
+		for (size_t i = 0; i < length; i++) {
+			ptr[i] = logf(ptr[i]);
+		}
+	} else if (ntype == numo_cDFloat) {
+		double *ptr = (double *)(nary_get_pointer_for_read_write(narray) + nary_get_offset(narray));
+		for (size_t i = 0; i < length; i++) {
+			ptr[i] = log(ptr[i]);
+		}
+	} else if (ntype == numo_cSComplex) {
+		float complex *ptr = (float complex *)(nary_get_pointer_for_read_write(narray) + nary_get_offset(narray));
+		for (size_t i = 0; i < length; i++) {
+			ptr[i] = clogf(ptr[i]);
+		}
+	} else if (ntype == numo_cDComplex) {
+		double complex *ptr = (double complex *)(nary_get_pointer_for_read_write(narray) + nary_get_offset(narray));
+		for (size_t i = 0; i < length; i++) {
+			ptr[i] = clog(ptr[i]);
+		}
+	} else {
+		rb_raise(rb_eException, "BUG: Unexpected type %"PRIsVALUE, ntype);
+	}
+
+	if (!was_inplace) {
+		UNSET_INPLACE(narray);
+	}
+
+	return narray;
+}
+
 static VALUE ruby_osc(VALUE self, VALUE wave_type, VALUE phi)
 {
 	enum wave_types wt = find_wave_type(SYM2ID(wave_type));
@@ -1328,6 +1381,9 @@ void Init_fast_sound(void)
 
 	rb_define_module_function(fast_sound, "smoothstep", ruby_smoothstep, 1);
 	rb_define_module_function(fast_sound, "smootherstep", ruby_smootherstep, 1);
+
+	// Mathematical functions that for some reason are missing from Numo::NArray
+	rb_define_module_function(fast_sound, "narray_log", ruby_narray_log, 1);
 
 	// Functions used when comparing C and Ruby's behavior for integer
 	// division (C rounds to zero, Ruby rounds downward) and modulus (-1 %
