8a2d34c23198e2fd64bd39acc67c3021f658929d
Mike Bourgeous <mike@mikebourgeous.com>
2021-12-10 18:43:34 -0700

    Added modulation wheel to fm_bass, got it sounding pretty bassy.

--- bin/fm_bass.rb
+++ bin/fm_bass.rb
@@ -1,7 +1,12 @@
 #!/usr/bin/env ruby
+# A very rough approximation of Solid Bass or Lately Bass from the classic
+# Yamaha FM synthesizers.
 
 require 'bundler/setup'
 require 'mb-sound'
+
+mod_constants = []
+constant_ranges = {}
 
 OSC_COUNT = ENV['OSC_COUNT']&.to_i || 1
 voices = OSC_COUNT.times.map { |i|
@@ -10,24 +15,34 @@ voices = OSC_COUNT.times.map { |i|
   bfreq = -> { base.dup.tap { |z| freq_constants << z } }
 
   # TODO: True FM/PM feedback instead of a duplicate copy of the oscillator
-  cenv = MB::Sound.adsr(0, 0.005, 0.0, 0.005, auto_release: false).named('cenv').db(30)
-  cenv2 = MB::Sound.adsr(0, 0.01, 0.0, 0.01, auto_release: false).named('cenv2').db(60)
-  c = cenv * MB::Sound.tone(bfreq.call * 2).at(1).pm(cenv2 * MB::Sound.tone(bfreq.call * 2).at(1).forever).forever
+  cenv = MB::Sound.adsr(0, 0.2, 0.0, 0.1, auto_release: false).named('cenv').db(30)
+  cenv2 = MB::Sound.adsr(0, 0.2, 0.0, 0.1, auto_release: false).named('cenv2').db(30)
+  fbconst = 4.constant.named('fbconst')
+  c = cenv * MB::Sound.tone(bfreq.call * 2).at(1).pm(cenv2 * fbconst * MB::Sound.tone(bfreq.call * 2).at(1).forever).forever
 
-  denv = MB::Sound.adsr(0, 0.005, 0.0, 0.005, auto_release: false).named('denv').db(50)
+  denv = MB::Sound.adsr(0, 0.3, 0.0, 0.35, auto_release: false).named('denv').db(30)
   d = denv * MB::Sound.tone(bfreq.call * 2 * 0.9996 - 0.22).at(1).forever.named('d')
 
-  eenv = MB::Sound.adsr(0, 2, 0.7, 2, auto_release: false).named('eenv')
-  cconst = 15.constant.named('cconst')
-  dconst = 13.constant.named('dconst')
+  eenv = MB::Sound.adsr(0, 2, 0.7, 1.5, auto_release: false).named('eenv')
+  cconst = 0.9.constant.named('cconst')
+  dconst = 0.7.constant.named('dconst')
   e = eenv.db * MB::Sound.tone(bfreq.call).at(1).pm(c * cconst + d * dconst).forever.named('e')
 
-  fenv = MB::Sound.adsr(0, 2, 0.8, 2, auto_release: false).named('fenv')
-  econst = 18.constant.named('econst')
+  fenv = MB::Sound.adsr(0, 2, 0.8, 1.5, auto_release: false).named('fenv')
+  econst = 2.constant.named('econst')
   f = fenv.db * MB::Sound.tone(bfreq.call).at(1).pm(e * econst).named('f')
+
+  mod_constants << fbconst
+  mod_constants << cconst
+  mod_constants << dconst
+  mod_constants << econst
 
   MB::Sound::MIDI::GraphVoice.new(f, amp_envelopes: [fenv], freq_constants: freq_constants)
 }
+
+mod_constants.each do |c|
+  constant_ranges[c] = c.constant..(c.constant * 2)
+end
 
 jack = MB::Sound::JackFFI[]
 output = jack.output(channels: 1, connect: [['system:playback_1', 'system:playback_2']])
@@ -36,6 +51,12 @@ pool = MB::Sound::MIDI::VoicePool.new(
   manager,
   voices
 )
+
+manager.on_cc(1) do |mod|
+  constant_ranges.each do |c, range|
+    c.constant = MB::M.scale(mod, 0..1, range)
+  end
+end
 
 output_chain = (pool * 20).softclip(0.8, 0.95)
 
