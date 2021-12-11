ffb5508d7897c48a633f8b805e455ae5e9880385
Mike Bourgeous <mike@mikebourgeous.com>
2021-12-10 17:58:42 -0700

    Name envelopes and oscillators in fm_bass.

--- bin/fm_bass.rb
+++ bin/fm_bass.rb
@@ -9,21 +9,22 @@ voices = OSC_COUNT.times.map { |i|
   freq_constants = []
   bfreq = -> { base.dup.tap { |z| freq_constants << z } }
 
-  cenv = MB::Sound.adsr(0, 0.005, 0.0, 0.005, auto_release: false).db(30)
-  cenv2 = MB::Sound.adsr(0, 0.01, 0.0, 0.01, auto_release: false).db(60)
+  # TODO: True FM/PM feedback instead of a duplicate copy of the oscillator
+  cenv = MB::Sound.adsr(0, 0.005, 0.0, 0.005, auto_release: false).named('cenv').db(30)
+  cenv2 = MB::Sound.adsr(0, 0.01, 0.0, 0.01, auto_release: false).named('cenv2').db(60)
   c = cenv * MB::Sound.tone(bfreq.call * 2).at(1).pm(cenv2 * MB::Sound.tone(bfreq.call * 2).at(1).forever).forever
 
-  denv = MB::Sound.adsr(0, 0.005, 0.0, 0.005, auto_release: false).db(50)
-  d = denv * MB::Sound.tone(bfreq.call * 2 * 0.9996 - 0.22).at(1).forever
+  denv = MB::Sound.adsr(0, 0.005, 0.0, 0.005, auto_release: false).named('denv').db(50)
+  d = denv * MB::Sound.tone(bfreq.call * 2 * 0.9996 - 0.22).at(1).forever.named('d')
 
-  eenv = MB::Sound.adsr(0, 2, 0.7, 2, auto_release: false)
-  cconst = 15
-  dconst = 13
-  e = eenv.db * MB::Sound.tone(bfreq.call).at(1).pm(c * cconst + d * dconst).forever
+  eenv = MB::Sound.adsr(0, 2, 0.7, 2, auto_release: false).named('eenv')
+  cconst = 15.constant.named('cconst')
+  dconst = 13.constant.named('dconst')
+  e = eenv.db * MB::Sound.tone(bfreq.call).at(1).pm(c * cconst + d * dconst).forever.named('e')
 
-  fenv = MB::Sound.adsr(0, 2, 0.8, 2, auto_release: false)
-  econst = 18.constant
-  f = fenv.db * MB::Sound.tone(bfreq.call).at(1).pm(e * econst)
+  fenv = MB::Sound.adsr(0, 2, 0.8, 2, auto_release: false).named('fenv')
+  econst = 18.constant.named('econst')
+  f = fenv.db * MB::Sound.tone(bfreq.call).at(1).pm(e * econst).named('f')
 
   MB::Sound::MIDI::GraphVoice.new(f, amp_envelopes: [fenv], freq_constants: freq_constants)
 }
