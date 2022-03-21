148cc5e6bff05a8de050ef69942cd88d405df83a
Mike Bourgeous <mike@mikebourgeous.com>
2022-03-20 18:25:02 -0600

    Account for smoothing in Constant spec.

--- spec/lib/mb/sound/constant_spec.rb
+++ spec/lib/mb/sound/constant_spec.rb
@@ -12,6 +12,33 @@ RSpec.describe(MB::Sound::Constant) do
     expect(c.sample(480)).to eq(Numo::SFloat.zeros(480).fill(123))
 
     c.constant = 1+1i
+    c.sample(480) # get past smoothstep
     expect(c.sample(480)).to eq(Numo::SComplex.zeros(480).fill(1+1i))
+  end
+
+  context 'when smooth is true' do
+    it 'smooths changes between values' do
+      c = MB::Sound::Constant.new(100)
+
+      c.constant = -100
+
+      result = c.sample(480)
+      expect(result.mean.round(2)).to eq(0)
+      expect(result.max.round(2)).to eq(100)
+      expect(result.min.round(2)).to eq(-100)
+      expect(result[0].round(2)).to eq(100)
+      expect(result[-1].round(2)).to eq(-100)
+      expect((result[239] + result[240]).round(2)).to eq(0)
+    end
+  end
+
+  context 'when smooth is false' do
+    it 'changes values instantly' do
+      c = MB::Sound::Constant.new(123, smooth: false)
+      expect(c.sample(480)).to eq(Numo::SFloat.zeros(480).fill(123))
+
+      c.constant = 1+1i
+      expect(c.sample(480)).to eq(Numo::SComplex.zeros(480).fill(1+1i))
+    end
   end
 end
