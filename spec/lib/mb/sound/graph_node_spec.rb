89dc5b93e3afe941335f34d98fc5ceb18d226f78
Mike Bourgeous <mike@mikebourgeous.com>
2022-04-10 00:20:26 -0600

    Rearranged some GraphNode specs.

--- spec/lib/mb/sound/graph_node_spec.rb
+++ spec/lib/mb/sound/graph_node_spec.rb
@@ -12,38 +12,6 @@ RSpec.describe(MB::Sound::GraphNode) do
     expect(graph.sample(5)).to eq(Numo::SFloat.zeros(5).fill(-2))
   end
 
-  it 'can apply softclipping' do
-    graph = (1.hz.square.at_rate(20).at(10) + 9.75).softclip(0.5, 1)
-    expect(graph.sample(10).mean).to be_between(0.5, 1.0)
-    expect(graph.sample(10).mean.round(6)).to eq(-0.25)
-  end
-
-  it 'can apply filtering' do
-    graph = 400.hz.at(1).filter(400.hz.lowpass(quality: 5))
-    expect(graph.sample(48000).max.round(6)).to eq(5)
-  end
-
-  it 'can create a dynamic filter' do
-    graph = 500.hz.filter(:highpass, cutoff: MB::Sound.adsr(0.2, 0.0, 1.0, 0.75, auto_release: 0.5) * 1000 + 100, quality: MB::Sound.adsr(0.3, 0.3, 1.0, 1.0, auto_release: 0.7) * -5 + 6)
-
-    # Ensure the correct types were created and stored
-    expect(graph).to be_a(MB::Sound::Filter::Cookbook::CookbookWrapper)
-    expect(graph.audio).to be_a(MB::Sound::Tone)
-    expect(graph.cutoff).to be_a(MB::Sound::GraphNode::Mixer)
-    expect(graph.quality).to be_a(MB::Sound::GraphNode::Mixer)
-
-    # Ensure 500Hz tone gets quieter as filter frequency rises
-    attack = graph.sample(2000).abs.max
-    graph.sample(14000)
-    sustain = graph.sample(10000).abs.max
-    expect(sustain).to be < (0.25 * attack)
-
-    # Expect tone to get louder as frequency falls again
-    graph.sample(12000)
-    release = graph.sample(2000).abs.max
-    expect(release).to be > (1.5 * sustain)
-  end
-
   it 'resets default durations on tones added or multiplied to a graph' do
     graph = (100.hz.for(2) + 33.hz.or_for(0.1) + 25.hz.or_for(0.1) - 11.hz.or_for(0.1)) * 10.hz.or_for(0.1) * 15.hz.or_for(0.1) - 5.hz.or_for(0.1)
 
@@ -124,6 +92,42 @@ RSpec.describe(MB::Sound::GraphNode) do
     end
   end
 
+  describe '#softclip' do
+    it 'can apply softclipping' do
+      graph = (1.hz.square.at_rate(20).at(10) + 9.75).softclip(0.5, 1)
+      expect(graph.sample(10).mean).to be_between(0.5, 1.0)
+      expect(graph.sample(10).mean.round(6)).to eq(-0.25)
+    end
+  end
+
+  describe '#filter' do
+    it 'can apply filtering' do
+      graph = 400.hz.at(1).filter(400.hz.lowpass(quality: 5))
+      expect(graph.sample(48000).max.round(6)).to eq(5)
+    end
+
+    it 'can create a dynamic filter' do
+      graph = 500.hz.filter(:highpass, cutoff: MB::Sound.adsr(0.2, 0.0, 1.0, 0.75, auto_release: 0.5) * 1000 + 100, quality: MB::Sound.adsr(0.3, 0.3, 1.0, 1.0, auto_release: 0.7) * -5 + 6)
+
+      # Ensure the correct types were created and stored
+      expect(graph).to be_a(MB::Sound::Filter::Cookbook::CookbookWrapper)
+      expect(graph.audio).to be_a(MB::Sound::Tone)
+      expect(graph.cutoff).to be_a(MB::Sound::GraphNode::Mixer)
+      expect(graph.quality).to be_a(MB::Sound::GraphNode::Mixer)
+
+      # Ensure 500Hz tone gets quieter as filter frequency rises
+      attack = graph.sample(2000).abs.max
+      graph.sample(14000)
+      sustain = graph.sample(10000).abs.max
+      expect(sustain).to be < (0.25 * attack)
+
+      # Expect tone to get louder as frequency falls again
+      graph.sample(12000)
+      release = graph.sample(2000).abs.max
+      expect(release).to be > (1.5 * sustain)
+    end
+  end
+
   describe '#clip' do
     # FIXME: the first sample is repeated without the with_phase option
     let(:cliposc) { 24000.hz.square.at(10).with_phase(0.0000001) }
@@ -145,6 +149,10 @@ RSpec.describe(MB::Sound::GraphNode) do
       expect(n.sample(4)).to eq(Numo::SFloat[10, 0.5, 10, 0.5])
     end
   end
+
+  pending '#smooth'
+  pending '#clip_rate'
+  pending '#multitap'
 
   describe '#coerce' do
     it 'allows signal nodes to be preceded by numeric values in multiplication' do
