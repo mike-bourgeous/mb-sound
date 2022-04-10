0df7a11ff4de7a91e1b90f0c2bdcd16f504d3550
Mike Bourgeous <mike@mikebourgeous.com>
2022-04-09 23:20:02 -0600

    Added a test of linear audio interpolation in the multi-tap delay.

--- spec/lib/mb/sound/graph_node/multitap_delay_spec.rb
+++ spec/lib/mb/sound/graph_node/multitap_delay_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe(MB::Sound::GraphNode::MultitapDelay) do
-  it 'can delay a single tap by a constant amount' do
-    dly = MB::Sound::GraphNode::MultitapDelay.new(5.constant(smoothing: false).named('Const'), 4.constant.named('Delay'), rate: 1)
+  it 'can delay a single tap by a graph constant' do
+    dly = MB::Sound::GraphNode::MultitapDelay.new(5.constant(smoothing: false).named('Const'), 4.constant, rate: 1)
     tap = dly.taps[0]
 
     expect(tap.sample(10)).to eq(Numo::SFloat[0, 0, 0, 0, 5, 5, 5, 5, 5, 5])
@@ -8,6 +8,22 @@ RSpec.describe(MB::Sound::GraphNode::MultitapDelay) do
 
     tap.find_by_name('Const').constant = 3
     expect(tap.sample(10)).to eq(Numo::SFloat[5, 5, 5, 5, 3, 3, 3, 3, 3, 3])
+  end
+
+  it 'interpolates values when delayed by a fractional sample' do
+    c = 0.constant(smoothing: false)
+    d = 0.5.constant(smoothing: false)
+    dly = MB::Sound::GraphNode::MultitapDelay.new(c, d, rate: 1)
+    tap = dly.taps[0]
+
+    expect(tap.sample(5)).to eq(Numo::SFloat[0, 0, 0, 0, 0])
+
+    c.constant = 1
+    expect(tap.sample(5)).to eq(Numo::SFloat[0.5, 1, 1, 1, 1])
+
+    d.constant = 0.25
+    c.constant = 2
+    expect(tap.sample(3)).to eq(Numo::SFloat[1.25, 2, 2])
   end
 
   pending 'multiple taps'
