16e0c68f8919d5ae5da66e5468171ee187b8a1c8
Mike Bourgeous <mike@mikebourgeous.com>
2024-06-16 00:20:01 -0600

    Added comment describing issue with analytic signal.

--- lib/mb/sound/acoustics_methods.rb
+++ lib/mb/sound/acoustics_methods.rb
@@ -3,15 +3,16 @@ module MB
     # Methods related to analyzing room acoustics, such as RT60
     module AcousticsMethods
       # Calculates the RT60 (reverberation time to -60dB), or decay time to the
-      # given +:level+ if given, from the highest amplitude peak within the
-      # +data+ (a 1D Numo::NArray or a Ruby Array of 1D Numo::NArray).  If
-      # +data+ is a Ruby Array, then the RT60 is calculated for each
-      # Numo::NArray within the Array.
+      # given +:level+ (as a ratio with the peak) if given, from the highest
+      # amplitude peak within the +data+ (a 1D Numo::NArray or a Ruby Array of
+      # 1D Numo::NArray).  If +data+ is a Ruby Array, then the RT60 is
+      # calculated for each Numo::NArray within the Array.
       #
       # The decay +:level+ may be anything from -inf to 1, exclusive, and
       # defaults to -60dB (0.001).  The sample +:rate+ defaults to 48000.
       #
-      # Returns the number of seconds to reach 
+      # Returns the number of seconds to reach the given ratio, or raises an
+      # error if that level is never reached.
       def rt60(data, level: -60.dB, rate: 48000)
         return data.map { |c| rt60(c, level: level) } if data.is_a?(Array)
         raise 'Data must be a 1D Numo::NArray' unless data.is_a?(Numo::NArray) && data.ndim == 1
@@ -21,6 +22,10 @@ module MB
         #   (geometric?  arithmetic probably), and convert to RT60
 
         # Convert to instantaneous magnitude form
+        # FIXME: analytic_signal drastically changes the envelope
+        # FIXME: e.g. analytic_signal(Numo::SFloat.logspace(0, -4, 48000) *
+        # FIXME: 123.Hz.sample(48000)) ends with analytic signal around 0.2, not
+        # FIXME: 0.0001.
         asig = analytic_signal(data).abs
         peak_idx = asig.max_index
         peak_val = asig[peak_idx]
