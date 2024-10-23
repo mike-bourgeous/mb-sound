506086edbae82ddcfd289d6069f2505f30e89b10
Mike Bourgeous <mike@mikebourgeous.com>
2024-10-22 22:16:17 -0600

    Couldn't help playing with the flanger.

--- bin/flanger.rb
+++ bin/flanger.rb
@@ -2,15 +2,30 @@
 # A simple flanger effect, to demonstrate using a signal node as a delay time.
 # (C)2022 Mike Bourgeous
 #
-# Cool pitch effect: SMOOTHING=0.5 bin/flanger.rb 0.035 0 3 2
+# Usage: $0 [delay_s [feedback [hz [depth0..1]]]] [filename]
+#    Or: $0 [filename]
+#
+# Environment variables:
+#    WAVE_TYPE - oscillator waveform name (e.g. sine, ramp, triangle, square)
+#    SMOOTHING - max delay change rate in seconds per second
+#    DRY - dry output level (default 1)
+#    WET - wet output level (default 1)
+#    SPREAD - LFO phase spread across channels (default 180)
+#
+# Cool effects (omit filename for realtime processing from Jack):
+#    Arpeggio: SMOOTHING=0.5 $0 sounds/transient_synth.flac 0.035 0 3 2
+#    Slow arp: SMOOTHING=1.5 $0 sounds/transient_synth.flac 0.15 0 3 2
+#    Metal drums: SMOOTHING=12.1 WET=1 DRY=0 $0 sounds/drums.flac 0.02 -0.3 343 -6
+#    Water drums: SMOOTHING=4 WET=1 DRY=0 $0 sounds/drums.flac 0.02 -0.3 46 6
+#    Space warp: SMOOTHING=10 WET=1 DRY=0 $0 sounds/drums.flac 0.2 -0.8 15 6
+#    Time warp: WET=1 DRY=0 $0 sounds/drums.flac 0.2 -0.8 0.3 6
 
 require 'bundler/setup'
 
 require 'mb/sound'
 
 if ARGV.include?('--help')
-  puts "Usage: [WAVE_TYPE=:ramp] \e[1m#{$0}\e[0m [delay_s [feedback [hz [depth0..1]]]] [filename]"
-  puts "   Or: \e[1m#{$0}\e[0m [filename]"
+  puts MB::U.read_header_comment.join.gsub('$0', "\e[1m#{$0}\e[0m")
   exit 1
 end
 
@@ -58,6 +73,8 @@ delay_smoothing2 = delay_smoothing
 dry_level = ENV['DRY']&.to_f || 1
 wet_level = ENV['WET']&.to_f || 1
 
+phase_spread = ENV['SPREAD']&.to_f || 180.0
+
 puts MB::U.highlight(
   wave_type: wave_type,
   delay: delay,
@@ -73,6 +90,7 @@ puts MB::U.highlight(
 # meters, or plots graphs of multiple nodes by name or reference
 
 begin
+  # FIXME: feedback delay includes buffer size
   # TODO: Abstract construction of a filter graph per channel
   paths = inputs.map.with_index { |inp, idx|
     # Feedback buffers, overwritten by later calls to #spy
@@ -80,7 +98,7 @@ begin
 
     lfo_freq = hz.constant.named('LFO Hz')
 
-    lfo = lfo_freq.tone.with_phase(idx * 2.0 * Math::PI / inputs.length).send(wave_type).forever.at(0..1)
+    lfo = lfo_freq.tone.with_phase(idx * phase_spread * Math::PI / (180.0 * (inputs.length - 1))).send(wave_type).forever.at(0..1)
 
     # Set up LFO depth control
     depthconst = depth.constant.named('Depth')
