8975cd05294a12a27dc147d161b37a1bccc71fce
Mike Bourgeous <mike@mikebourgeous.com>
2024-12-28 13:18:04 -0700

    Fully implemented multi-reader mode in CircularBuffer.

--- lib/mb/sound/circular_buffer.rb
+++ lib/mb/sound/circular_buffer.rb
@@ -2,7 +2,24 @@ module MB
   module Sound
     # A 1D circular buffer implemented around Numo::SFloat, Numo::DFloat,
     # Numo::SComplex, or Numo::DComplex, supporting reads and writes of
-    # arbitrary sizes.
+    # arbitrary sizes.  Also supports multiple readers with different offsets,
+    # useful for fanout or delays.
+    #
+    # Single-reader example:
+    #
+    #     cbuf = MB::Sound::CircularBuffer.new(buffer_size: 100)
+    #     cbuf.write(Numo::SFloat[1,2,3])
+    #     cbuf.read(2) # => Numo::SFloat[1,2]
+    #
+    # Multi-reader example:
+    #
+    #     cbuf = MB::Sound::CircularBuffer.new(buffer_size: 100)
+    #     r1 = cbuf.reader
+    #     r2 = cbuf.reader
+    #     cbuf.write(Numo::SComplex[1i,2i,3i])
+    #     r1.read(2) # => Numo::SComplex[1i,2i]
+    #     r2.read(3) # => Numo::SComplex[1i,2i,3i]
+    #     r1.read(1) # => Numo::SComplex[3i]
     class CircularBuffer
       include BufferHelper
 
@@ -12,36 +29,63 @@ module MB
       # Thrown when more data is read than has been written.
       class BufferUnderflow < RuntimeError; end
 
-      # XXX multi-reader idea; see also GraphNode::MultitapDelay
-      class Reader
-        attr_reader :read_pos
+      # Thrown when #read is called after #reader, or vice versa.
+      class ReaderModeError < RuntimeError; end
 
-        def initialize(cbuf:, index:, read_pos:)
+      # Single reader with an independent read pointer as returned by #reader.
+      class Reader
+        include BufferHelper
+
+        # The number of samples available for reading from this Reader's
+        # position.
+        attr_reader :length
+        alias count length
+
+        # For internal use by CircularBuffer#reader.  Creates a new Reader at
+        # the given read position with the given initial length of available
+        # samples.
+        def initialize(cbuf:, index:, read_pos:, length:)
           @cbuf = cbuf
           @index = index
           @read_pos = read_pos
           @tmpbuf = nil
-        end
+          @length = length
 
-        def length
-          (@cbuf.write_pos - @read_pos) % @cbuf.buffer_size
+          setup_buffer(length: cbuf.buffer_size, complex: cbuf.complex, double: cbuf.double)
         end
 
         def read(count)
-          raise BufferUnderflow, "Reader #{@index} has #{length}, but #{count} requested" if count > length
+          # TODO: should we allow short reads (return less than requested)?
+          if count > @length
+            raise BufferUnderflow, "Read of size #{count} is greater than #{@length} available samples " \
+              "on #{@index < 0 ? 'default reader' : "reader #{@index}"}"
+          end
 
-          @cbuf.direct_read(pos: @read_pos, count: count, target: nil).tap {
+          setup_buffer(length: @cbuf.buffer_size, complex: @cbuf.complex, double: @cbuf.double)
+
+          # Return an empty NArray if asked to read nothing, even if the buffer is empty
+          return @buf.class[] if count == 0
+
+          @cbuf.direct_read(pos: @read_pos, count: count, target: @buf).tap { |v|
             @read_pos = (@read_pos + count) % @cbuf.buffer_size
+            @length -= v.length
           }
+        end
+
+        # For internal use by CircularBuffer.  Increments this reader's internal length value.
+        def wrote(count)
+          raise "BUG: wrote past read position on reader #{@index}" if (count + @length) >
+          @length += count
+        end
+
+        # Returns true if there are no samples available for this reader to read.
+        def empty?
+          @length == 0
         end
       end
 
       # Maximum buffer size
       attr_reader :buffer_size
-
-      # Number of values available for reading
-      attr_reader :length
-      alias count length
 
       # Whether the buffer is real (false) or complex (true)
       attr_reader :bufcomplex
@@ -65,53 +109,64 @@ module MB
 
         @buffer_size = buffer_size
 
-        @read_pos = 0
         @write_pos = 0
-        @length = 0
 
+        # In multi-reader mode, we store all readers in @readers.  In
+        # single-reader mode, we store one reader in @r0.
         @readers = []
+        @r0 = nil
 
-        setup_buffer(length: @buffer_size, complex: complex, temp: true, double: double)
+        setup_buffer(length: @buffer_size, complex: complex, temp: false, double: double)
       end
 
-      # XXX multi-reader idea/experiment
+      # Creates and returns a new Reader for this buffer in multi-reader mode,
+      # with its own independent read pointer.  The read position defaults to
+      # the current write position at time of reader creation (e.g. an empty
+      # buffer), minus the delay specified by +delay_samples+.
+      #
+      # Call this before calling any other methods, as most other methods will
+      # switch the buffer into single-reader mode.
+      #
+      # Raises ReaderModeError if the buffer is in single-reader mode (e.g.
+      # #read, #write, or another method has been called first).
+      #
+      # Raises ArgumentError if +delay_samples+ would put the read pointer
+      # behind the write pointer.
       def reader(delay_samples = 0)
-        # FIXME: include read pos in exception check or something
-        raise 'Cannot delay more than the buffer size' if delay_samples > @buffer_size
+        raise ReaderModeError, 'Buffer is in single-reader mode.  You must call #reader before calling any other methods.' unless @r0.nil?
 
-        Reader.new(cbuf: self, index: @readers.length, read_pos: (@read_pos - delay_samples) % @buffer_size).tap { |r|
+        raise ArgumentError, 'Cannot delay more than the buffer size' if delay_samples > @buffer_size
+
+        Reader.new(
+          cbuf: self,
+          index: @readers.count,
+          read_pos: (@write_pos - delay_samples) % @buffer_size,
+          length: delay_samples
+        ).tap { |r|
           @readers << r
         }
       end
 
-      # Returns the next +count+ samples of the buffer.
+      # Returns the next +count+ samples of the buffer when in single-reader
+      # mode.
+      #
+      # Raises ReaderModeError if the buffer is in multi-reader mode (e.g. if
+      # #reader has been called).
       #
       # Raises BufferUnderflow if +count+ is greater than #length.
       def read(count)
-        # TODO: XXX Just have an internal reader by default and call that?
-
-        # TODO: should we allow short reads (return less than requested)?
-        if count > @length
-          raise BufferUnderflow, "Read of size #{count} is greater than #{@length} available samples"
-        end
-
-        # Return an empty NArray if asked to read nothing, even if the buffer is empty
-        return @buf.class[] if count == 0
-
-        MB::M.circular_read(@buf, @read_pos, count, target: @tmpbuf)[0...count].tap {
-          @read_pos = (@read_pos + count) % @buffer_size
-          @length -= count
-        }
+        default_reader.read(count)
       end
 
       # For internal use by Reader.  Does a direct circular read from the
-      # internal buffer at the given position.
+      # internal buffer at the given position, storing the result in +:target+,
+      # and windowing the target to the given +:count+.
       def direct_read(pos:, count:, target:)
-        MB::M.circular_read(@buf, pos, count, target: target)
+        MB::M.circular_read(@buf, pos, count, target: target)[0...count].not_inplace!
       end
 
-      # Appends the given +narray+ to the circular buffer, returning the
-      # new count of available samples to read.
+      # Appends the given +narray+ to the circular buffer, returning the new
+      # count of available samples to read from the furthest-behind reader.
       #
       # Raises BufferOverflow without writing any data if there's not enough
       # room for the given +narray+ in the buffer.
@@ -129,24 +184,51 @@ module MB
           raise BufferOverflow, "Write of size #{narray.length} is greater than #{available} space available"
         end
 
-        # TODO: XXX take readers buffers into account
         expand_buffer(narray, grow: false)
 
         MB::M.circular_write(@buf, narray, @write_pos)
         @write_pos = (@write_pos + narray.length) % @buffer_size
-        @length += narray.length
+
+        @readers&.each do |r|
+          r.wrote(narray.length)
+        end
+
+        @r0&.wrote(narray.length)
+
+        @readers&.map(&:length)&.max || length
       end
 
-      # Returns the available space left in the circular buffer.  See also
-      # #length.
+      # Returns the number of samples available for reading in single-reader
+      # mode.  Switches to single-reader mode if no mode has been set.
+      #
+      # Raises ReaderModeError if the buffer is in multi-reader mode.
+      def length
+        default_reader.length
+      end
+      alias count length
+
+      # Returns the available space left for writing in the circular buffer,
+      # whether in single-reader or multi-reader mode.  Switches to
+      # single-reader mode if no mode has been set.  See also #length.
       def available
-        # TODO: XXX take readers into account
-        @buffer_size - @length
+        @buffer_size - (@readers.max_by { |v| v.length }&.length || default_reader.length)
       end
 
-      # Returns true if there are no samples in the circular buffer.
+      # Returns true if there are no samples in the circular buffer in any
+      # readers, whether in single-reader (#read) or multi-reader (#reader)
+      # mode.
       def empty?
-        @length == 0
+        @readers.empty? ? default_reader.empty? : @readers.all?(&:empty?)
+      end
+
+      private
+
+      # Returns the default reader if it exists, or switches to single-reader
+      # mode by creating the default reader.  Raises an error if we're already
+      # in multi-reader mode.
+      def default_reader
+        raise ReaderModeError, 'Reader is in multi-reader mode.  Do not call #read or #length after creating a reader with #reader.' unless @readers.empty?
+        @r0 ||= Reader.new(cbuf: self, index: -1, read_pos: 0, length: @write_pos)
       end
     end
   end
--- spec/lib/mb/sound/circular_buffer_spec.rb
+++ spec/lib/mb/sound/circular_buffer_spec.rb
@@ -1,6 +1,84 @@
 RSpec.describe(MB::Sound::CircularBuffer, :aggregate_failures) do
   let(:cbuf) { MB::Sound::CircularBuffer.new(buffer_size: 7) }
 
+  describe '::Reader' do
+    describe '#read' do
+      it 'can adapt to changes in data type' do
+        r1 = cbuf.reader
+        r2 = cbuf.reader(1)
+        cbuf.write(Numo::SFloat[1,2,3,4])
+        expect(r1.read(2)).to be_a(Numo::SFloat).and eq(Numo::SFloat[1,2])
+        cbuf.write(Numo::SComplex[-1i])
+
+        expect(r2.read(2)).to be_a(Numo::SComplex).and eq(Numo::SComplex[0, 1])
+        expect(r1.read(3)).to be_a(Numo::SComplex).and eq(Numo::SComplex[3, 4, -1i])
+      end
+
+      it 'raises an error if trying to read more data than is available' do
+        r1 = cbuf.reader
+        cbuf.write(Numo::SFloat[1,2])
+        r2 = cbuf.reader
+        expect { r2.read(1) }.to raise_error(MB::Sound::CircularBuffer::BufferUnderflow)
+        expect { r1.read(3) }.to raise_error(MB::Sound::CircularBuffer::BufferUnderflow)
+        expect(r1.read(1)).to eq(Numo::SFloat[1])
+      end
+
+      it 'returns an empty NArray if asked for zero samples' do
+        r1 = cbuf.reader
+        expect(r1.read(0)).to be_a(Numo::SFloat).and(eq(Numo::SFloat[]))
+
+        cbuf.write(Numo::SComplex[1])
+        expect(r1.read(0)).to be_a(Numo::SComplex).and(eq(Numo::SComplex[]))
+      end
+    end
+
+    describe '#empty?' do
+      it 'returns false if there are samples to read' do
+        r1 = cbuf.reader
+        cbuf.write(Numo::SFloat[1, 2])
+        r2 = cbuf.reader
+        cbuf.write(Numo::SFloat[3])
+        r3 = cbuf.reader(1)
+
+        expect(r1.empty?).to eq(false)
+        expect(r2.empty?).to eq(false)
+        expect(r3.empty?).to eq(false)
+      end
+
+      it 'returns true if there are no samples to read' do
+        r1 = cbuf.reader
+        expect(r1.empty?).to eq(true)
+
+        cbuf.write(Numo::SFloat[1, 2])
+        r2 = cbuf.reader
+        expect(r2.empty?).to eq(true)
+      end
+    end
+
+    describe '#length' do
+      it 'starts at zero if there is no delay' do
+        r1 = cbuf.reader
+        expect(r1.length).to eq(0)
+
+        cbuf.write(Numo::SFloat[1])
+        r2 = cbuf.reader
+        expect(r2.length).to eq(0)
+      end
+
+      it 'increases when the buffer is written' do
+        r1 = cbuf.reader
+        r2 = cbuf.reader(3)
+        expect {
+          cbuf.write(Numo::SFloat[-1, -2])
+        }.to change { r1.length }.by(2).and(change { r2.length }.by(2))
+      end
+
+      it 'starts at the delay if a delay is given' do
+        expect(cbuf.reader(4).length).to eq(4)
+      end
+    end
+  end
+
   it 'maintains length and available values while reading and writing data' do
     expect(cbuf.buffer_size).to eq(7)
 
@@ -94,14 +172,145 @@ RSpec.describe(MB::Sound::CircularBuffer, :aggregate_failures) do
       cbuf.write(Numo::DComplex[-1i])
       expect(cbuf.read(0)).to eq(Numo::DComplex[])
     end
+
+    it 'raises an error if #reader has been called' do
+      cbuf.reader
+      expect { cbuf.read(0) }.to raise_error(MB::Sound::CircularBuffer::ReaderModeError)
+    end
   end
 
   describe '#write' do
-    it 'raises an error if writing more data than will fit in the buffer' do
-      expect { cbuf.write(Numo::SFloat[1,2,3,4,5,6,7,8]) }.to raise_error(MB::Sound::CircularBuffer::BufferOverflow)
+    context 'in single-reader mode' do
+      it 'raises an error if writing more data than will fit in the buffer' do
+        expect { cbuf.write(Numo::SFloat[1,2,3,4,5,6,7,8]) }.to raise_error(MB::Sound::CircularBuffer::BufferOverflow)
 
-      cbuf.write(Numo::DComplex[1,2])
-      expect { cbuf.write(Numo::SFloat[1,2,3,4,5,6]) }.to raise_error(MB::Sound::CircularBuffer::BufferOverflow)
+        cbuf.write(Numo::DComplex[1,2])
+        expect { cbuf.write(Numo::SFloat[1,2,3,4,5,6]) }.to raise_error(MB::Sound::CircularBuffer::BufferOverflow)
+      end
+    end
+
+    context 'in multi-reader mode' do
+      it 'can write up to the position of the slowest reader' do
+        r1 = cbuf.reader
+        r2 = cbuf.reader(3)
+
+        expect { cbuf.write(Numo::SFloat[1,2,3,4]) }.not_to raise_error(MB::Sound::CircularBuffer::BufferOverflow)
+      end
+
+      it 'raises an error if the write would pass any of the read positions' do
+        r1 = cbuf.reader
+        r2 = cbuf.reader(3)
+
+        expect { cbuf.write(Numo::SFloat[1,2,3,4,5]) }.to raise_error(MB::Sound::CircularBuffer::BufferOverflow)
+      end
+    end
+  end
+
+  describe '#reader' do
+    it 'can create multiple readers' do
+      r1 = cbuf.reader
+      r2 = cbuf.reader
+      r3 = cbuf.reader
+
+      cbuf.write(Numo::SFloat[1,2,3,4,5,6,7])
+
+      expect(r1.read(7)).to eq(Numo::SFloat[1,2,3,4,5,6,7])
+      expect(r2.read(4)).to eq(Numo::SFloat[1,2,3,4])
+      expect(r3.read(3)).to eq(Numo::SFloat[1,2,3])
+      expect(r2.read(2)).to eq(Numo::SFloat[5,6])
+      expect(r3.read(1)).to eq(Numo::SFloat[4])
+    end
+
+    it 'creates readers at the write pointer' do
+      r1 = cbuf.reader
+      cbuf.write(Numo::SFloat[5,4,3])
+      r2 = cbuf.reader
+      cbuf.write(Numo::SFloat[-1,-2])
+      expect(r1.read(4)).to eq(Numo::SFloat[5,4,3,-1])
+      expect(r2.read(2)).to eq(Numo::SFloat[-1,-2])
+    end
+
+    it 'can create a delayed reader' do
+      r1 = cbuf.reader(5)
+      expect(r1.length).to eq(5)
+      expect(cbuf.available).to eq(2)
+      expect { cbuf.write(Numo::SFloat[1,2,3]) }.to raise_error(MB::Sound::CircularBuffer::BufferOverflow)
+    end
+
+    it 'raises an error if #write was called before calling #reader' do
+      cbuf.write(Numo::SFloat[1])
+      expect { cbuf.reader }.to raise_error(MB::Sound::CircularBuffer::ReaderModeError)
+    end
+
+    it 'raises an error if #read has been called' do
+      cbuf.read(0)
+      expect { cbuf.reader }.to raise_error(MB::Sound::CircularBuffer::ReaderModeError)
+    end
+
+    it 'raises an error if #length has been called' do
+      cbuf.length
+      expect { cbuf.reader }.to raise_error(MB::Sound::CircularBuffer::ReaderModeError)
+    end
+
+    it 'raises an error if #available was called first' do
+      cbuf.available
+      expect { cbuf.reader }.to raise_error(MB::Sound::CircularBuffer::ReaderModeError)
+    end
+
+    it 'raises an error if #empty? was called first' do
+      cbuf.empty?
+      expect { cbuf.reader }.to raise_error(MB::Sound::CircularBuffer::ReaderModeError)
+    end
+  end
+
+  describe '#length' do
+    context 'in multi-reader mode' do
+      it 'raises an error' do
+        cbuf.reader
+        expect { cbuf.length }.to raise_error(MB::Sound::CircularBuffer::ReaderModeError)
+      end
+    end
+
+    context 'in single-reader mode' do
+      it 'returns samples available for writing' do
+        expect(cbuf.length).to eq(0)
+        cbuf.write(Numo::SFloat[1,2,3])
+        expect(cbuf.length).to eq(3)
+      end
+    end
+  end
+
+  describe '#available' do
+    context 'in multi-reader mode' do
+      it 'returns the samples that can be written without passing the furthest-behind reader' do
+        cbuf.reader
+        expect(cbuf.available).to eq(7)
+        cbuf.reader(2)
+        expect(cbuf.available).to eq(5)
+        cbuf.reader(7)
+        expect(cbuf.available).to eq(0)
+      end
+    end
+
+    context 'in single-reader mode' do
+      it 'returns the number of samples that can be written' do
+        expect(cbuf.available).to eq(7)
+        cbuf.write(Numo::SFloat[1,2,3])
+        expect(cbuf.available).to eq(4)
+      end
+    end
+  end
+
+  describe '#empty?' do
+    context 'in multi-reader mode' do
+      it 'returns true if all readers are empty' do
+      end
+    end
+
+    context 'in single-reader mode' do
+      it 'returns true if no samples are available for reading' do
+
+      end
     end
   end
 
