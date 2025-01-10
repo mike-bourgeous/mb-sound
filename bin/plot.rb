873dae785f098d6502ca085c64523f9819a589d2
Mike Bourgeous <mike@mikebourgeous.com>
2025-01-10 14:59:25 -0700

    Added missing script used by IOLogger test.

---
+++ bin/plot.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# Plots a given audio file.
+# Usage: $0 filename
+
+require 'bundler/setup'
+
+require 'mb/util'
+
+require 'mb/sound'
+
+graphical = !!ARGV.delete('--graphical')
+
+if ARGV.length != 1 || ARGV.include?('--help')
+  MB::U.print_header_help
+  exit 1
+end
+
+data = MB::Sound.read(ARGV[0])
+loop do
+  # TODO: there's got to be a better way to respond to window size changes
+  MB::Sound.plot(data, samples: data[0].length, graphical: graphical)
+  break unless graphical
+  sleep 2
+end
