635c1eeb51e4afa22911465ee08c0222a5c0b530
Mike Bourgeous <mike@mikebourgeous.com>
2025-04-04 11:21:42 -0600

    Add a cursory test of the CLI.

---
+++ spec/bin/sound_spec.rb
@@ -0,0 +1,12 @@
+RSpec.describe('bin/sound.rb/', :aggregate_failures) do
+  it 'can start up and plot a sine wave' do
+    raw_text = `printf "plot 123.hz\nexit\n" | bin/sound.rb`
+    result = $?
+    output = MB::U.remove_ansi(raw_text)
+
+    expect(output).to include('Welcome to the'), 'shows the welcome text'
+    expect(output).to include('sound.rb MB::Sound'), 'displays the right prompt'
+    expect(output).to include('123.0'), 'shows the frequency being plotted'
+    expect(output).to include('--------------'), 'draws the plot'
+  end
+end
