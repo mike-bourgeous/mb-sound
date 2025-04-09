8b2c658a11d9105b90e47edcc55231f0b7245ae1
Mike Bourgeous <mike@mikebourgeous.com>
2025-04-09 16:37:22 -0600

    Enable branch coverage in SimpleCov.

--- spec/simplecov_helper.rb
+++ spec/simplecov_helper.rb
@@ -3,9 +3,10 @@ require 'securerandom'
 require 'simplecov'
 
 SimpleCov.start do
-  SimpleCov.command_name "#{$0} #{$$} #{SecureRandom.uuid}"
-  SimpleCov.formatter SimpleCov::Formatter::SimpleFormatter
-  SimpleCov.minimum_coverage 0
+  command_name "#{$0} #{$$} #{SecureRandom.uuid}"
+  formatter SimpleCov::Formatter::SimpleFormatter
+  minimum_coverage 0
+  enable_coverage :branch
 end
 
 # This require line makes sure the original script file is processed by simplecov
