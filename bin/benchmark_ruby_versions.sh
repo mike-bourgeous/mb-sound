f676afb2bf474077fff36f7d7c35db8587f0c4ac
Mike Bourgeous <mike@mikebourgeous.com>
2025-04-21 16:34:26 -0600

    Fix handling of missing Ruby version or gemset in benchmark loop.

--- bin/benchmark_ruby_versions.sh
+++ bin/benchmark_ruby_versions.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+set -e
+
 for version in 2.7 3.0 3.1 3.2 3.3 3.4; do
 	printf "\n\e[1;33m----------------\nRuby ${version}\n----------------\e[0m\n\n"
 
-	printf "\e[1mbundle install\e[0m\n"
+	printf "\n\e[1mruby install\e[0m\n"
+	rvm install ${version}
+	rvm ${version} do rvm gemset create mb-sound
+
+	printf "\n\e[1mbundle install\e[0m\n"
 	rvm ${version}@mb-sound do bundle install
 
-	printf "\e[1mrake clean compile\e[0m\n"
+	printf "\n\e[1mrake clean compile\e[0m\n"
 	rvm ${version}@mb-sound do rake clean compile
 	
-	printf "\e[1mbenchmark\e[0m\n"
+	printf "\n\e[1mbenchmark\e[0m\n"
 	rvm ${version}@mb-sound do bin/node_graph_benchmark.rb --bench
 done
