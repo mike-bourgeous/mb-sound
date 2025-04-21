eaac6371a2b7bfa57e232e13a7f664ea923c8411
Mike Bourgeous <mike@mikebourgeous.com>
2025-04-21 15:53:06 -0600

    Tweak song tonal balance slightly.

--- bin/node_graph_benchmark.rb
+++ bin/node_graph_benchmark.rb
@@ -18,7 +18,7 @@ abenv = MB::Sound::ADSREnvelope.new(attack_time: 60, decay_time: 30, sustain_lev
 a = 100.hz.complex_square.forever.at(-13.db).filter(1500.hz.lowpass(quality: 0.5))
 b = 150.hz.ramp.forever.at(-15.db).filter(2600.hz.lowpass(quality: 0.5))
 
-ab = (a + b).softclip(0.05, 0.2) * 1.hz.drumramp.lfo.at(2..0.1).filter(30.hz.lowpass) * abenv
+ab = (a + b).softclip(0.05, 0.2) * 3.db * 1.hz.drumramp.lfo.at(2..0.1).filter(30.hz.lowpass) * abenv
 
 cenv = MB::Sound::ADSREnvelope.new(attack_time: 90, decay_time: 60, sustain_level: 1, release_time: 30, rate: 48000)
 
@@ -27,14 +27,14 @@ c = (
   250.hz.complex_triangle.forever.at(-3.db).softclip(0.05, 0.5).filter(1900.hz.lowpass1p) * 0.1.hz.lfo.at(0..1).with_phase(Math::PI)
 ).softclip(0.05, 0.25) * 10.db * cenv
 
-denv = MB::Sound::ADSREnvelope.new(attack_time: 4, decay_time: 156, sustain_level: 1, release_time: 20, rate: 48000)
+denv = MB::Sound::ADSREnvelope.new(attack_time: 4, decay_time: 156, sustain_level: 1, release_time: 10, rate: 48000)
 
 d = (
   50.hz.triangle.at(-3.db).forever.filter(150.hz.lowpass1p) *
   4.hz.drumramp.lfo.at(0..-30).db.filter(50.hz.lowpass)
 ).softclip(0.005, 0.25) * 10.db * denv
 
-drumenv = MB::Sound::ADSREnvelope.new(attack_time: 10, decay_time: 160, sustain_level: 1, release_time: 10, rate: 48000)
+drumenv = MB::Sound::ADSREnvelope.new(attack_time: 10, decay_time: 160, sustain_level: 1, release_time: 20, rate: 48000)
 
 hat = 10000.hz.noise.filter(9000.hz.highpass).filter(15000.hz.lowpass) * 8.hz.drumramp.lfo.at(-4..-25).filter(100.hz.lowpass).db
 kick = 50.hz.at(-3.db).fm(2.hz.drumramp.at(90.to_db..-60).db.filter(100.hz.lowpass)) * 2.hz.drumramp.at(0..-30).db.filter(100.hz.lowpass)
