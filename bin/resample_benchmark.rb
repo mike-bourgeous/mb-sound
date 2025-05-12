cc8c17c77c83407b6a34b7fb2a90afccec8e941d
Mike Bourgeous <mike@mikebourgeous.com>
2025-05-12 00:20:56 -0600

    Benchmark a couple of buffer methods to show any future improvements.

--- bin/resample_benchmark.rb
+++ bin/resample_benchmark.rb
@@ -12,6 +12,27 @@ MB::U.sigquit_backtrace
 SAMPLE_COUNT = ENV['SAMPLES']&.to_i || 48000 * 180
 
 Benchmark.bmbm do |bench|
+  bench.report("ruby_zoh single sample") do
+    100.hz.forever.at_rate(441)
+      .resample(17000, mode: :ruby_zoh)
+      .sample(SAMPLE_COUNT)
+  end
+
+  bench.report("ruby_zoh sample loop") do
+    node = 100.hz.forever.at_rate(441)
+      .resample(17000, mode: :ruby_zoh)
+
+    (SAMPLE_COUNT.to_f / 716).ceil.times do
+      node.sample(716)
+    end
+  end
+
+  bench.report("ruby_zoh multi_sample()") do
+    100.hz.forever.at_rate(441)
+      .resample(17000, mode: :ruby_zoh)
+      .multi_sample(716, (SAMPLE_COUNT.to_f / 716).ceil)
+  end
+
   [233, 800, 4000].each do |bufsize|
     MB::Sound::GraphNode::Resample::MODES.each do |mode|
       upsample = 100.hz.forever.at_rate(1234).resample(5432, mode: mode)
