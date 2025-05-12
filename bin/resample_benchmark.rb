dd065a1376a912338b754808497c7dd6b01fafd8
Mike Bourgeous <mike@mikebourgeous.com>
2025-05-11 23:52:57 -0600

    Added a benchmark of resampling algorithms and buffer sizes.

---
+++ bin/resample_benchmark.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+require 'bundler/setup'
+
+require 'benchmark'
+
+require 'mb-util'
+require 'mb-sound'
+
+MB::U.sigquit_backtrace
+
+SAMPLE_COUNT = 48000 * 180
+
+Benchmark.bmbm do |bench|
+  [233, 800, 4000].each do |bufsize|
+    MB::Sound::GraphNode::Resample::MODES.each do |mode|
+      upsample = 100.hz.forever.at_rate(1234).resample(5432, mode: mode)
+      downsample = 100.hz.forever.at_rate(17000).resample(5432, mode: mode)
+
+      bench.report("#{mode.inspect}@#{bufsize} upsampling") do
+        (SAMPLE_COUNT.to_f / bufsize).ceil.times do
+          upsample.sample(bufsize)
+        end
+      end
+
+      bench.report("#{mode.inspect}@#{bufsize} downsampling") do
+        (SAMPLE_COUNT.to_f / bufsize).ceil.times do
+          downsample.sample(bufsize)
+        end
+      end
+    end
+  end
+end
