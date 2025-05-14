d5a1129e530c838ac997aad3450c8be82b94daae
Mike Bourgeous <mike@mikebourgeous.com>
2025-05-14 00:52:38 -0600

    Add some tests of the quantization graph node.

--- spec/lib/mb/sound/graph_node/quantize_spec.rb
+++ spec/lib/mb/sound/graph_node/quantize_spec.rb
@@ -1,8 +1,33 @@
-RSpec.describe(MB::Sound::GraphNode::Quantize) do
-  pending 'can quantize to a numeric value'
-  pending 'can quantize to another graph node'
-  pending 'does not quantize if the numeric value is zero'
-  pending 'does not quantize if the graph node output is zero'
+RSpec.describe(MB::Sound::GraphNode::Quantize, :aggregate_failures) do
+  let (:data1) { MB::Sound::ArrayInput.new(data: Numo::SFloat[0.49, 0.5, 0.51, -0.49, -0.5, -0.51]) }
+  let (:complex_data) { MB::Sound::ArrayInput.new(data: Numo::DComplex[0.49, 0.5i, 0.51, -0.49i, -0.5, -0.51i, -0.25+0.8i]) }
+
+  it 'can quantize to a numeric increment' do
+    expect(data1.dup.quantize(0.25).sample(6)).to eq(Numo::SFloat[0.5, 0.5, 0.5, -0.5, -0.5, -0.5])
+    expect(data1.dup.quantize(1).sample(6)).to eq(Numo::SFloat[0, 1, 1, 0, -1, -1])
+  end
+
+  it 'does not matter if the quantization increment is negative' do
+    expect(data1.dup.quantize(-0.25).sample(6)).to eq(Numo::SFloat[0.5, 0.5, 0.5, -0.5, -0.5, -0.5])
+    expect(data1.dup.quantize(-1).sample(6)).to eq(Numo::SFloat[0, 1, 1, -0, -1, -1])
+  end
+
+  it 'can quantize to another graph node' do
+    expect(data1.dup.quantize(MB::Sound::ArrayInput.new(data: Numo::SFloat[0.33, 0.33, 0.5, 0.5, -1, 1])).sample(6)).to eq(Numo::SFloat[0.33, 0.66, 0.5, -0.5, -1, -1])
+  end
+
+  it 'does not quantize if the numeric value is zero' do
+    expect(data1.dup.quantize(0).sample(6)).to eq(data1.dup.sample(6))
+  end
+
+  it 'does not quantize if the graph node output is zero' do
+    expect(data1.dup.quantize(MB::Sound::ArrayInput.new(data: Numo::SFloat[0, 0, 0, 1, 1, 0.6])).sample(6)).to eq(Numo::SFloat[0.49, 0.5, 0.51, 0, -1, -0.6])
+  end
+
+  it 'can quantize complex inputs' do
+    expect(complex_data.dup.quantize(0.33).sample(7)).to eq(Numo::DComplex[0.33, 0.66i, 0.66, -0.33i, -0.66, -0.66i, -0.33+0.66i])
+  end
+
   pending 'nans and infinities'
   pending 'differing sample rates between upstream and increment'
 end
