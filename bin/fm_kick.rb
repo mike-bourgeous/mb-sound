5092a9c41773f1ea3990a2988f69dd5da3996731
Mike Bourgeous <mike@mikebourgeous.com>
2025-05-21 23:54:43 -0600

    Added room sound.

--- bin/fm_kick.rb
+++ bin/fm_kick.rb
@@ -13,8 +13,8 @@ end
 
 # Generates a node graph and GraphVoice to synthesize a kick
 def kicker
-  pitch_decay = 0.06
-  decay_time = 0.15
+  pitch_decay = 0.13
+  decay_time = 0.18
 
   # TODO: the node graph and GraphVoice really need some concept of i/o ports
   # and configurable parameters.
@@ -35,19 +35,55 @@ def kicker
   falling_sine = (attack_env + bfreq.call * (0.06 * pitch_env + 0.97)).tone.at(1).pm(noise_source)
   falling_sine_amp = falling_sine.adsr(0.0001, decay_time, 0, decay_time, log: 60)
 
-  final = falling_sine_amp.peq({
+  sub = falling_sine_amp.peq({
     30.hz => 9.db,
     95.hz => [6.db, 0.5],
     600.hz => [-20.db, 1.5],
     9000.hz => [25.db, 1.5],
   })
 
+  ################################################
+
+  boom_sine_decay = 0.4
+  boom_noise_decay = 0.7
+
+  boom_noise_cutoff = 10000.constant.named('Noise cutoff')
+  boom_noise_gain = 2500.constant.named('Noise gain')
+  boom_noise = 10000.hz.ramp.noise
+    .at(1)
+    .filter(:lowpass, cutoff: boom_noise_cutoff)
+    .adsr(0.0001, boom_noise_decay, 0.0, boom_noise_decay, log: 40)
+
+  boom_sine = 143.hz.at(1).fm(boom_noise * boom_noise_gain)
+    .adsr(0.008, boom_sine_decay, 0.0, boom_sine_decay, log: 50)
+    .adsr(0.008, boom_sine_decay, 0.0, boom_sine_decay)
+
+  boom = boom_sine.peq({
+    20.hz => [-20.db, 1],
+    60.hz => [11.db, 0.6],
+    100.hz => [6.db, 0.6],
+    133.hz => [-9.db, 0.3],
+    180.hz => [3.db, 2],
+    350.hz => 4.db,
+    950.hz => [-12.db, 3],
+    6000.hz => [-4.db, 3],
+    12000.hz => [-1.db, 1],
+    45.hz => [3.db, 0.1],
+    95.hz => [2.db, 0.1],
+  })
+
+  ################################################
+
+
+  final = sub * 1 + boom * 0.1
+
   MB::Sound::MIDI::GraphVoice.new(
     final,
     freq_constants: freq_constants
   ).named('Kick').tap { |v|
-    v.on_cc(1, 'Attack Hz', range: 0..2000, relative: false)
+    #v.on_cc(1, 'Attack Hz', range: 0..2000, relative: false)
     #v.on_cc(1, 'Noise cutoff', range: 0..10000, relative: false)
+    #v.on_cc(1, 'Noise gain', range: 0..10000, relative: false)
   }
 end
 
@@ -88,8 +124,7 @@ begin
     end
     areak if midi&.empty? && !pool.active? && !repeat
 
-    puts pool.find_by_name('Attack Hz').constant # XXX
-    puts pool.find_by_name('Noise cutoff').constant # XXX
+    #puts pool.find_by_name('Noise gain').constant
 
     if pry_next
       require 'pry-byebug'
