9860df6bff0fd762850123ed823c1aeddbf1528e
Mike Bourgeous <mike@mikebourgeous.com>
2025-12-13 13:28:24 -0700

    Restore self-playing stereo graph.

--- bin/stereo_graph_example.rb
+++ bin/stereo_graph_example.rb
@@ -3,13 +3,9 @@
 require 'bundler/setup'
 require 'mb-sound'
 
-midi = MB::Sound.midi
-
-q = (midi.frequency * 2.001).tone.at(1).with_phase(Math::PI/3) + 0.1.hz.lfo.at(1) * (midi.frequency * 1.001).tone.at(Math::PI)
+q = 120.1.hz.at(1).with_phase(Math::PI/3) + 0.1.hz.lfo.at(1) * 60.hz.lfo.at(Math::PI)
 a = (
-  (
-    ((midi.frequency * 6.001).tone.pm(q) + (midi.frequency * 8.001).tone.pm(q)) + (midi.frequency * 0.501).tone.ramp.at(0.3).filter(:lowpass, cutoff: 0.23.hz.lfo.at(130..2500))
-  ) * midi.env
+  (360.3.hz.pm(q) + 481.4.hz.pm(q)).oversample(3).forever + 30.hz.ramp.at(0.3).filter(:lowpass, cutoff: 0.23.hz.lfo.at(130..2500))
 ).softclip.oversample(2)
 
 left = a.delay(seconds: 0.4.hz.lfo.at(0..0.01), feedback: -0.5, dry: 1, smoothing: false).softclip
