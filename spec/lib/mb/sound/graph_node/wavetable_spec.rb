f01ebfd861b133212d84ed182d2c47e8192ab78d
Mike Bourgeous <mike@mikebourgeous.com>
2025-12-13 21:50:56 -0700

    Fix phase test for wavetable node (missing a comma but syntax valid?).

--- spec/lib/mb/sound/graph_node/wavetable_spec.rb
+++ spec/lib/mb/sound/graph_node/wavetable_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe(MB::Sound::GraphNode::Wavetable) do
-  let(:data) { Numo::SFloat[[1, -1, 1, 1, -1 -1], [0, 1, -1, 1, 0, -1]] }
+  let(:data) { Numo::SFloat[[1, -1, 1, 1, -1, -1], [0, 1, -1, 1, 0, -1]] }
 
   it 'can be created' do
     expect(120.hz.ramp.wavetable(wavetable: data, number: 0.constant)).to be_a(MB::Sound::GraphNode::Wavetable)
