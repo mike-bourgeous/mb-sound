69ca4a9760201bf9606b706fcb705839dc96ec95
Mike Bourgeous <mike@mikebourgeous.com>
2025-12-13 21:07:53 -0700

    Added specs for loading and saving wavetables.

--- spec/lib/mb/sound/wavetable_spec.rb
+++ spec/lib/mb/sound/wavetable_spec.rb
@@ -3,20 +3,46 @@ RSpec.describe(MB::Sound::Wavetable, aggregate_failures: true) do
     Numo::SFloat[
       [1, 2, 3, 4, 5],
       [5, -3, 3, -1, 2],
-      [-1, 1, 0, 0.1, 0.2],
+      [-1, 1, 0, 3, -4],
     ]
   }
 
-  pending '.load_wavetable'
-  pending '.save_wavetable'
+  describe '.load_wavetable' do
+    it 'can load an existing wavetable' do
+      data = MB::Sound::Wavetable.load_wavetable('spec/test_data/short_wavetable.flac')
+      expect(MB::M.round(data, 5)).to eq(MB::M.round(table / 5, 6))
+    end
+  end
+
+  describe '.save_wavetable' do
+    it 'can write a wavetable to disk' do
+      data = Numo::SFloat[
+        [0.5, -0.5, 0],
+        [0.1, 0.2, -0.3],
+        [-0.75, 0.25, 0.75],
+      ]
+
+      name = 'tmp/wavetable_save.flac'
+
+      FileUtils.mkdir_p('tmp/')
+      File.unlink(name) rescue nil
+
+      MB::Sound::Wavetable.save_wavetable(name, data)
+
+      metadata = {}
+      result = MB::Sound.read(name, metadata_out: metadata)[0]
+      expect(MB::M.round(result, 6)).to eq(Numo::SFloat[0.5, -0.5, 0, 0.1, 0.2, -0.3, -0.75, 0.25, 0.75])
+      expect(metadata[:mb_sound_wavetable_period]&.to_i).to eq(3)
+    end
+  end
 
   describe '.wavetable_lookup' do
     it 'can perform lookups based on arrays' do
-      number = Numo::SFloat[0, 1.0 / 3.0, 2.0 / 3.0, -2.0 / 3.0]
-      phase = Numo::SFloat[0, 0.4, 0.6, 1.2]
+      number = Numo::SFloat[0, 1.0 / 3.0, 2.0 / 3.0, -2.0 / 3.0, 1.0 / 6.0]
+      phase = Numo::SFloat[0, 0.4, 0.6, 1.2, 0.1]
 
       result = MB::Sound::Wavetable.wavetable_lookup(wavetable: table, number: number, phase: phase)
-      expect(MB::M.round(result, 5)).to eq(MB::M.round(Numo::SFloat[1, 3, 0.1, -3], 5))
+      expect(MB::M.round(result, 5)).to eq(MB::M.round(Numo::SFloat[1, 3, 3, -3, 1.25], 5))
     end
   end
 
@@ -26,7 +52,7 @@ RSpec.describe(MB::Sound::Wavetable, aggregate_failures: true) do
       expect(MB::Sound::Wavetable.outer_lookup(wavetable: table, number: 1.0 / 3.0, phase: 0).round(6)).to eq(5)
       expect(MB::Sound::Wavetable.outer_lookup(wavetable: table, number: 2.0 / 3.0, phase: 0).round(6)).to eq(-1)
     end
-    
+
     it 'can retrieve exact columns in the wavetable' do
       expect(MB::Sound::Wavetable.outer_lookup(wavetable: table, number: 0, phase: 0).round(6)).to eq(1)
       expect(MB::Sound::Wavetable.outer_lookup(wavetable: table, number: 0, phase: 0.2).round(6)).to eq(2)
@@ -52,9 +78,9 @@ RSpec.describe(MB::Sound::Wavetable, aggregate_failures: true) do
 
     it 'can interpolate between rows' do
       expect(MB::Sound::Wavetable.outer_lookup(wavetable: table, number: 1.0 / 6.0, phase: 0.8).round(6)).to eq(3.5)
-      expect(MB::Sound::Wavetable.outer_lookup(wavetable: table, number: 3.0 / 6.0, phase: 0.6).round(6)).to eq(-0.45)
+      expect(MB::Sound::Wavetable.outer_lookup(wavetable: table, number: 3.0 / 6.0, phase: 0.6).round(6)).to eq(1.0)
     end
-    
+
     it 'can interpolate between columns' do
       expect(MB::Sound::Wavetable.outer_lookup(wavetable: table, number: 0, phase: 0.1).round(6)).to eq(1.5)
       expect(MB::Sound::Wavetable.outer_lookup(wavetable: table, number: 1.0 / 3.0, phase: 0.1).round(6)).to eq(1.0)
