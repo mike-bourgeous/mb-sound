332766d1a519d0529de4ce2ade01bbd38eaa3acd
Mike Bourgeous <mike@mikebourgeous.com>
2025-12-27 11:17:02 -0700

    Fix make_wavetable spec to work with OptionParser change.

--- spec/bin/make_wavetable_spec.rb
+++ spec/bin/make_wavetable_spec.rb
@@ -11,11 +11,11 @@ RSpec.describe('bin/make_wavetable.rb', aggregate_failures: true) do
     expect(MB::U.remove_ansi(text)).to match(/120.*|.*B2/)
 
     info = MB::Sound::FFMPEGInput.parse_info(name)
-    expect(info[:streams][0][:duration_ts]).to eq((48000 / 120) * 100)
+    expect(info[:streams][0][:duration_ts]).to eq((48000 / 120) * 10)
   end
 
   it 'can change the table size' do
-    text = `bin/make_wavetable.rb --quiet sounds/piano_120hz_b2.flac #{name} 30`
+    text = `bin/make_wavetable.rb --quiet sounds/piano_120hz_b2.flac #{name} --size=30`
     expect(MB::U.remove_ansi(text)).to match(/120.*|.*B2/)
 
     info = MB::Sound::FFMPEGInput.parse_info(name)
