d02c363e0cc096912a9d0a66237086e29fee890f
Mike Bourgeous <mike@mikebourgeous.com>
2026-01-02 19:06:57 -0700

    Test each JIT type available in each Ruby version.

--- bin/benchmark_ruby_versions.rb
+++ bin/benchmark_ruby_versions.rb
@@ -66,54 +66,42 @@ results = VERSION_LIST.map { |version|
 
   puts "\n\e[1;33m------------------\nBenchmark ruby #{version}\n------------------\e[0m\n\n"
 
-  # TODO: allow selecting script to run
-  if $enabled_benchmarks.include?(:node_graph)
-    puts "\n\e[1mRunning node graph benchmark\e[0m\n\n"
+  jit_types = `rvm #{version}@mb-sound do ruby --help | grep -Eo -- "--.?jit"`.strip.lines.map { |l| l.strip.sub('--', '') }.uniq
+  jit_types.delete('jit') if jit_types.length > 1
 
-    puts "\n\e[1mbenchmark \e[36m#{version} \e[31mwithout jit\e[0m\n"
-    node_graph_csv = `RUBYOPT='' rvm #{version}@mb-sound do bin/songs/node_graph_benchmark.rb --bench`
-    check_success
+  jit_types.unshift(nil)
 
-    puts "\n\e[1mbenchmark \e[36m#{version} \e[32mwith jit\e[0m\n"
-    node_graph_jit_csv = `RUBYOPT=--jit rvm #{version}@mb-sound do bin/songs/node_graph_benchmark.rb --bench`
-    check_success
+  version_results = {}
+
+  jit_types.each do |jit|
+    if jit
+      puts "\n\e[1mbenchmark \e[36m#{version} \e[32mwith #{jit}\e[0m\n"
+      opts = "--#{jit}"
+    else
+      puts "\n\e[1mbenchmark \e[36m#{version} \e[31mwithout jit\e[0m\n"
+      opts = "''"
+    end
+
+    if $enabled_benchmarks.include?(:node_graph)
+      puts "\n\e[1mRunning node graph benchmark\e[0m\n\n"
+      version_results[:"node_graph_#{jit || 'no_jit'}"] = `RUBYOPT=#{opts} rvm #{version}@mb-sound do bin/songs/node_graph_benchmark.rb --bench`
+      check_success
+    end
+
+    if $enabled_benchmarks.include?(:resampling)
+      MB::U.headline 'Running resampling benchmark'
+      version_results[:"resampling_#{jit || 'no_jit'}"] = `RUBYOPT=#{opts} rvm #{version}@mb-sound do bin/resample_benchmark.rb`
+      check_success
+    end
+
+    if $enabled_benchmarks.include?(:wavetable)
+      MB::U.headline 'Running wavetable benchmark'
+      version_results[:"wavetable_#{jit || 'no_jit'}"] = `RUBYOPT=#{opts} rvm #{version}@mb-sound do bin/wavetable_benchmark.rb`
+      check_success
+    end
   end
 
-  if $enabled_benchmarks.include?(:resampling)
-    MB::U.headline 'Running resampling benchmark'
-
-    puts "\n  \e[1mbenchmark \e[36m#{version} \e[31mwithout jit\e[0m\n"
-    resampling_csv = `RUBYOPT='' rvm #{version}@mb-sound do bin/resample_benchmark.rb`
-    check_success
-
-    puts "\n  \e[1mbenchmark \e[36m#{version} \e[32mwith jit\e[0m\n"
-    resampling_jit_csv = `RUBYOPT=--jit rvm #{version}@mb-sound do bin/resample_benchmark.rb`
-    check_success
-  end
-
-  if $enabled_benchmarks.include?(:wavetable)
-    MB::U.headline 'Running wavetable benchmark'
-
-    puts "\n\e[1mbenchmark \e[36m#{version} \e[31mwithout jit\e[0m\n"
-    wavetable_csv = `RUBYOPT='' rvm #{version}@mb-sound do bin/wavetable_benchmark.rb`
-    check_success
-
-    puts "\n\e[1mbenchmark \e[36m#{version} \e[32mwith jit\e[0m\n"
-    wavetable_jit_csv = `RUBYOPT=--jit rvm #{version}@mb-sound do bin/wavetable_benchmark.rb`
-    check_success
-  end
-
-  [
-    version,
-    {
-      node_graph: node_graph_csv,
-      node_graph_jit: node_graph_jit_csv,
-      resampling: resampling_csv,
-      resampling_jit: resampling_jit_csv,
-      wavetable: wavetable_csv,
-      wavetable_jit: wavetable_jit_csv,
-    },
-  ]
+  [ version, version_results ]
 }.to_h
 
 MB::U.headline("RESULTS")
