439bf341a8626499557f8c0d76bbdb838c3d0fdd
Mike Bourgeous <mike@mikebourgeous.com>
2026-05-28 14:36:51 -0600

    Don't skip envelope start if an event appears to have come from the past

--- lib/mb/sound/graph_node/midi_dsl/midi_envelope.rb
+++ lib/mb/sound/graph_node/midi_dsl/midi_envelope.rb
@@ -32,7 +32,7 @@ module MB
 
               # TODO: support multiple events per buffer
               # FIXME: timestamp will be in base sample rate; need to account for oversampling
-              self.time = -timestamp
+              self.time = -timestamp if timestamp > 0
               puts "Set envelope time to #{self.time}" # XXX
             elsif number == @number
               release
