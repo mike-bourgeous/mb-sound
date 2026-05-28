9aa5df1201e47dd30cb3e9c1693de6a530a39976
Mike Bourgeous <mike@mikebourgeous.com>
2026-05-28 16:21:26 -0600

    Convert fm_bass.rb to the new script and synth helpers and MIDI DSL.

--- bin/synths/fm_bass.rb
+++ bin/synths/fm_bass.rb
@@ -7,54 +7,36 @@ require 'mb-sound'
 
 MB::U.sigquit_backtrace
 
-OSC_COUNT = ENV['OSC_COUNT']&.to_i || 1
-repeat = !!ARGV.delete('--loop')
+MB::Sound.synth_script { |input|
+  s = MB::Sound.synth(input) { |midi|
+    # TODO: simpler way to convert MIDI number to frequency or tone
+    base = MB::Sound::Oscillator.calc_freq(midi.number.smooth(seconds: 0.1))
+    base2x = base * 2
+    mod = midi.cc(1, range: 1.0..2.0)
 
-# TODO: Build an abstraction around switching between Jack and MIDI files for
-# input, and FLAC files for output, for use by all synthesizers
-jack = MB::Sound::JackFFI[]
-output = jack.output(channels: 1, connect: [['system:playback_1', 'system:playback_2']])
-midi = MB::Sound::MIDI::MIDIFile.new(ARGV[0]) if ARGV[0]&.end_with?('.mid') # TODO: Add a clock source based on jackd frames
-manager = MB::Sound::MIDI::Manager.new(jack: jack, input: midi, connect: ARGV[0])
+    # TODO: True FM/PM feedback instead of a duplicate copy of the oscillator
+    cenv = midi.env(0, 0.2, 0.0, 0.1).named('cenv').db(30)
+    c = cenv * base2x.tone.complex_sine.at(1).pm(cenv * mod * base2x.tone.at(1))
 
-voices = OSC_COUNT.times.map { |i|
-  base = 440.constant
-  freq_constants = []
-  bfreq = -> { 2 ** base.dup.tap { |z| freq_constants << z }.log2.smooth(seconds: 0.1) }
+    denv = midi.env(0, 0.3, 0.0, 0.35).named('denv').db(30)
+    d = denv * (base2x * 0.9996 - 0.22).tone.complex_sine.at(1).named('d')
 
-  # TODO: True FM/PM feedback instead of a duplicate copy of the oscillator
-  cenv = MB::Sound.adsr(0, 0.2, 0.0, 0.1, auto_release: false).named('cenv').db(30)
-  cenv2 = MB::Sound.adsr(0, 0.2, 0.0, 0.1, auto_release: false).named('cenv2').db(30)
-  fbconst = 4.constant.named('Feedback')
-  c = cenv * MB::Sound.tone(bfreq.call * 2).complex_sine.at(1).pm(cenv2 * fbconst * MB::Sound.tone(bfreq.call * 2).at(1).forever).forever
+    # FIXME: sounds are too quiet below ~80 velocity
+    eenv = midi.env(0, 2, 0.7, 0.5).named('eenv').db
+    e = eenv * base.tone.complex_sine.at(1).pm(mod * (c + d)).named('e')
 
-  denv = MB::Sound.adsr(0, 0.3, 0.0, 0.35, auto_release: false).named('denv').db(30)
-  d = denv * MB::Sound.tone(bfreq.call * 2 * 0.9996 - 0.22).complex_sine.at(1).forever.named('d')
+    fenv = midi.env(0, 2, 0.8, 0.5).named('fenv').db
+    f = fenv * base.tone.complex_sine.at(1).pm(e * mod).named('f')
 
-  eenv = MB::Sound.adsr(0, 2, 0.7, 0.5, auto_release: false).named('eenv')
-  cconst = 0.9.constant.named('C')
-  dconst = 0.7.constant.named('D')
-  e = eenv.db * MB::Sound.tone(bfreq.call).complex_sine.at(1).pm(c * cconst + d * dconst).forever.named('e')
-
-  fenv = MB::Sound.adsr(0, 2, 0.8, 0.5, auto_release: false).named('fenv')
-  econst = 2.constant.named('E')
-  f = fenv.db * MB::Sound.tone(bfreq.call).complex_sine.at(1).pm(e * econst).named('f')
-
-  g = f.real.filter(15000.hz.lowpass).oversample(4, mode: :libsamplerate_fastest) # Try to cut down on aliasing chalkboard noise
-
-  final = g + g.delay(seconds: 0.1) * 0.125
-
-  MB::Sound::MIDI::GraphVoice.new(
-    final,
-    amp_envelopes: [fenv],
-    freq_constants: freq_constants,
-    manager: manager
-  ).named('FM Bass').tap { |v|
-    v.on_cc(1, 'Feedback', range: 1.0..2.0)
-    v.on_cc(1, 'C', range: 1.0..2.0)
-    v.on_cc(1, 'D', range: 1.0..2.0)
-    v.on_cc(1, 'E', range: 1.0..2.0)
+    f.real
   }
+
+  # Reduce aliasing noise
+  s = s.softclip(0.8, 0.95)
+    .filter(15000.hz.lowpass)
+    .oversample(4, mode: :libsamplerate_fastest)
+
+  s + s.delay(seconds: 0.1) * 0.125
 }
 
 voices[0].open_graphviz
