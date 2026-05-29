7003e3d5c2a9f83cc90cc809ee61080fca8e71b8
Mike Bourgeous <mike@mikebourgeous.com>
2026-05-29 17:30:43 -0600

    Convert wavetable_bass.rb to the new synth helpers.

--- bin/synths/wavetable_bass.rb
+++ bin/synths/wavetable_bass.rb
@@ -5,45 +5,72 @@ require 'bundler/setup'
 require 'mb-util'
 require 'mb-sound'
 
-MB::U.sigquit_backtrace
-
 DETUNE_CENTS = 5
 DETUNE_SEMIS = 0.01 * DETUNE_CENTS
 DETUNE_RANGE = (2 ** -DETUNE_SEMIS)..(2 ** DETUNE_SEMIS)
 PORTAMENTO_TIME = 0.1 # TODO: control with midi CC for portamento
 
-if ARGV[0]&.downcase&.end_with?('.mid') && File.readable?(ARGV[0])
-  # FIXME: exit when the MIDI file has ended
-  midi = MB::Sound.midi_file(ARGV[0], speed: ENV['MIDI_SPEED']&.to_f || 1)
-else
-  midi = MB::Sound.midi
-end
-
-cc1 = midi.cc(1).filter(:lowpass, cutoff: 10, quality: 0.5)
-cc2 = midi.cc(2, range: 1..10).filter(:lowpass, cutoff: 10, quality: 0.5)
-cc4 = midi.cc(4).filter(:lowpass, cutoff: 10, quality: 0.5)
-
 MB::U.headline('Loading wavetables...')
+synthwave = MB::Sound::Wavetable.load_wavetable('sounds/drums.flac', slices: 10)
+shaperwave = MB::Sound::Wavetable.load_wavetable('sounds/synth0.flac', slices: 10)
 
-synth = MB::Sound::Wavetable.load_wavetable('sounds/drums.flac', slices: 10)
-shaper = MB::Sound::Wavetable.load_wavetable('sounds/synth0.flac', slices: 10)
+MB::Sound.synth_script { |input|
+  MB::U.headline('Building synth...')
 
-voices = Array.new(4) do
-  (
-    midi.env(0.003, 0.05, 0.5, 0.3) * cc2 *
-    (
-      midi.frequency(rand(DETUNE_RANGE)).filter(:lowpass, cutoff: 1.0 / PORTAMENTO_TIME, quality: 0.5).tone.ramp.at(2).wavetable(wavetable: synth, number: cc1).filter(:lowpass, cutoff: 5000, quality: 0.4) +
-        midi.frequency(rand(DETUNE_RANGE)).filter(:lowpass, cutoff: 1.0 / PORTAMENTO_TIME, quality: 0.5).tone.triangle.at(0.5)
-    )
-  ).softclip
-    .*(2)
-    .wavetable(wavetable: shaper, number: cc4)
-    .filter(:highpass, cutoff: 10, quality: 0.7)
-end
+  s1 = MB::Sound.synth(input, osc_count: 1) { |midi|
+    cc1 = midi.cc(1).filter(:lowpass, cutoff: 10, quality: 0.5)
+    cc2 = midi.cc(2, range: 1..10).filter(:lowpass, cutoff: 10, quality: 0.5)
+    cc4 = midi.cc(4).filter(:lowpass, cutoff: 10, quality: 0.5)
 
-l = (0.5 * voices[0] + voices[1]).filter(:lowpass, cutoff: 15000, quality: 0.25).softclip #XXX .oversample(2)
-r = (0.5 * voices[2] + voices[3]).filter(:lowpass, cutoff: 15000, quality: 0.25).softclip #XXX .oversample(2)
+    a = midi
+      .frequency(rand(DETUNE_RANGE))
+      .filter(:lowpass, cutoff: 1.0 / PORTAMENTO_TIME, quality: 0.5).named('A Portamento')
+      .tone.ramp.at(2).named('A Phase')
+      .wavetable(wavetable: synthwave, number: cc1).named('A Wavetable')
+      .filter(:lowpass, cutoff: 5000, quality: 0.4).named('A Filter')
 
-MB::U.headline('Begin play!')
+    b = midi
+      .frequency(rand(DETUNE_RANGE))
+      .filter(:lowpass, cutoff: 1.0 / PORTAMENTO_TIME, quality: 0.5).named('B Portamento')
+      .tone.triangle.at(0.5).named('B')
 
-MB::Sound.play([l, r], plot: false)
+    env = midi.env(0.003, 0.05, 0.5, 0.3)
+
+    sum = (a + b) * cc2 * env
+
+    (sum.softclip * 2)
+      .wavetable(wavetable: shaperwave, number: cc4)
+  }.filter(:highpass, cutoff: 10, quality: 0.7)
+
+  s2 = MB::Sound.synth(input, osc_count: 1) { |midi|
+    cc1 = midi.cc(1).filter(:lowpass, cutoff: 10, quality: 0.5)
+    cc2 = midi.cc(2, range: 1..10).filter(:lowpass, cutoff: 10, quality: 0.5)
+    cc4 = midi.cc(4).filter(:lowpass, cutoff: 10, quality: 0.5)
+
+    a = midi
+      .frequency(rand(DETUNE_RANGE))
+      .filter(:lowpass, cutoff: 1.0 / PORTAMENTO_TIME, quality: 0.5).named('A Portamento')
+      .tone.ramp.at(2).named('A Phase')
+      .wavetable(wavetable: synthwave, number: cc1).named('A Wavetable')
+      .filter(:lowpass, cutoff: 5000, quality: 0.4).named('A Filter')
+
+    b = midi
+      .frequency(rand(DETUNE_RANGE))
+      .filter(:lowpass, cutoff: 1.0 / PORTAMENTO_TIME, quality: 0.5).named('B Portamento')
+      .tone.triangle.at(0.5).named('B')
+
+    env = midi.env(0.003, 0.05, 0.5, 0.3)
+
+    sum = (a + b) * cc2 * env
+
+    (sum.softclip * 2)
+      .wavetable(wavetable: shaperwave, number: cc4)
+  }.filter(:highpass, cutoff: 10, quality: 0.7)
+
+  l = s1.filter(:lowpass, cutoff: 15000, quality: 0.25).softclip
+  r = s2.filter(:lowpass, cutoff: 15000, quality: 0.25).softclip
+
+  MB::U.headline('Begin play!')
+
+  [l, r]
+}
